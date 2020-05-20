02e7d52b67f2f2f4bd58629db99b634208f0225d
SivaKanukolluQA <siva.kanukollu@hmcts.net>
2020-05-20 13:45:54 +0100

    AM-739: Smoke Test Serenity Setup.

--- src/smokeTest/java/uk/gov/hmcts/reform/roleassignment/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/roleassignment/SmokeTest.java
@@ -1,20 +1,16 @@
 package uk.gov.hmcts.reform.roleassignment;
 
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import uk.gov.hmcts.reform.roleassignment.apihelper.Constants;
-import uk.gov.hmcts.reform.roleassignment.utils.IdamUtils;
+
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @SpringBootTest
 @RunWith(SpringIntegrationSerenityRunner.class)
@@ -22,7 +18,7 @@ import uk.gov.hmcts.reform.roleassignment.utils.IdamUtils;
 public class SmokeTest extends BaseTest {
 
     @Test
-    public void sample_test_setuo_should_receive_response_for_role_assignment_api() {
+    public void sample_test_setup_should_receive_response_for_role_assignment_api() {
 
         RestAssured.baseURI = "http://localhost:4096";
         RestAssured.useRelaxedHTTPSValidation();
