3c8351e50e8f4eeb8f1a5b7b009cbe02dfc5b171
SivaKanukolluQA <siva.kanukollu@hmcts.net>
2020-05-20 13:35:02 +0100

    AM-739: Smoke Serenity Test Setup.

--- src/smokeTest/java/uk/gov/hmcts/reform/roleassignment/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/roleassignment/SmokeTest.java
@@ -21,51 +21,19 @@ import uk.gov.hmcts.reform.roleassignment.utils.IdamUtils;
 //@ConfigurationProperties()
 public class SmokeTest extends BaseTest {
 
-    @Value("${idam.s2s-auth.totp_secret}")
-    String secret;
-    @Value("${idam.s2s-auth.microservice}")
-    String microService;
-    @Value("${idam.s2s-auth.url}")
-    String s2sUrl;
-    @Value("${roleAssignmentUrl}")
-    String roleAssignmentUrl;
-
-    IdamUtils idamUtils = new IdamUtils();
-
-    String username = "befta.caseworker.2.solicitor.2@gmail.com";
-    String password = "Pa55word11";
-    String userToken = idamUtils.getIdamOauth2Token(username, password);
-    String documentId = "00000000-0000-0000-0000-000000000000";
-
-
     @Test
-    public void should_receive_response_for_a_get_document_meta_data() {
+    public void sample_test_setuo_should_receive_response_for_role_assignment_api() {
 
-        RestAssured.baseURI = roleAssignmentUrl + "/cases/documents/" + documentId;
+        RestAssured.baseURI = "http://localhost:4096";
         RestAssured.useRelaxedHTTPSValidation();
 
         Response response = SerenityRest
             .given()
             .relaxedHTTPSValidation()
             .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-            .header(Constants.SERVICE_AUTHORIZATION2, Constants.BEARER + getServiceAuth())
-            .header(Constants.AUTHORIZATION, Constants.BEARER + userToken)
             .when()
             .get("/")
             .andReturn();
-        response.then().assertThat().statusCode(HttpStatus.NOT_FOUND.value())
-                .body("message", Matchers.equalTo("Resource not found " + documentId));
-    }
-
-    private String getServiceAuth() {
-        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
-            s2sUrl = "http://localhost:4502";
-        } else {
-            s2sUrl = "http://rpe-service-auth-provider-aat.service.core-compute-aat.internal";
-        }
-
-        return new BaseTest().authTokenGenerator(secret, microService,
-                                                 generateServiceAuthorisationApi(s2sUrl)
-                                                ).generate();
+        response.then().assertThat().statusCode(HttpStatus.OK.value());
     }
 }
