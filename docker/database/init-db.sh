308f0a16c028beedc8a15884eb006c852b596e86
Nitin <nitin.sahu@hmcts.net>
2020-05-21 13:33:07 +0100

    AM-746 Minor updates

--- docker/database/init-db.sh
+++ docker/database/init-db.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-echo "Strting init.db.sh.........."
-
-echo $ROLE_ASSIGNMENT_DB_USERNAME
-echo $ROLE_ASSIGNMENT_DB_PASSWORD
-
+if [ -z "$ROLE_ASSIGNMENT_DB_USERNAME" ] || [ -z "$ROLE_ASSIGNMENT_DB_PASSWORD" ]; then
+  echo "ERROR: Missing environment variable. Set value for both 'ROLE_ASSIGNMENT_DB_USERNAME' and 'ROLE_ASSIGNMENT_DB_PASSWORD'."
+  exit 1
+fi
 
 # Create role and database
 psql -v ON_ERROR_STOP=1 --username postgres --set USERNAME=$ROLE_ASSIGNMENT_DB_USERNAME --set PASSWORD=$ROLE_ASSIGNMENT_DB_PASSWORD <<-EOSQL
