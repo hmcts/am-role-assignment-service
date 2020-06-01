08e2a68f0afc2b3011e8c3d23236c4f10bbb7813
dharmendra8186 <dharmendra.singh@hmcts.net>
2020-06-01 19:10:10 +0100

    AM-764  restructure the entities model to persist the records in db.

---
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/v1/V1.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.roleassignment.v1;
+
+public final class V1 {
+
+    private V1() {
+    }
+
+
+    public static final class Error {
+        private Error() {
+        }
+
+        public static final String INVALID_REQUEST = "Request is not valid as per validation rule";
+
+    }
+
+}
