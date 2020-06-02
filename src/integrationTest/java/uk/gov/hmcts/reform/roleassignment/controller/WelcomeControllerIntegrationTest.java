786803194bf5fdf2b488f20895ade19b5c5b79c2
Nitin <nitin.sahu@hmcts.net>
2020-06-02 16:45:49 +0100

    AM-782:Spike for Integration test - Enabled IT

--- src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/controller/WelcomeControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/controller/WelcomeControllerIntegrationTest.java
@@ -1,29 +1,23 @@
-/*
 package uk.gov.hmcts.reform.roleassignment.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.nio.charset.Charset;
 
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
-import java.nio.charset.Charset;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-
-
-@SpringBootTest
-@RunWith(SpringRunner.class)
-public class WelcomeControllerIntegrationTest {
+public class WelcomeControllerIntegrationTest extends BaseTest {
 
     private static final Logger logger = LoggerFactory.getLogger(WelcomeControllerIntegrationTest.class);
 
@@ -58,4 +52,3 @@ public class WelcomeControllerIntegrationTest {
             "Service is unavailable:", 200, 200);
     }
 }
-*/
