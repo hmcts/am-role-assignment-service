608d928ec6318051b3819330f8e84104929182f0
ConorH994 <conor.halpin@hmcts.net>
2020-06-05 13:43:00 +0100

    resolving conflicts

--- src/main/java/uk/gov/hmcts/reform/roleassignment/domain/service/common/PersistenceService.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/domain/service/common/PersistenceService.java
@@ -5,38 +5,21 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.roleassignment.data.roleassignment.HistoryEntity;
 import uk.gov.hmcts.reform.roleassignment.data.roleassignment.HistoryRepository;
-import uk.gov.hmcts.reform.roleassignment.data.roleassignment.HistoryStatusEntity;
-import uk.gov.hmcts.reform.roleassignment.data.roleassignment.HistoryStatusRepository;
-import uk.gov.hmcts.reform.roleassignment.data.roleassignment.RequestRepository;
-import uk.gov.hmcts.reform.roleassignment.data.roleassignment.RequestStatusRepository;
-import uk.gov.hmcts.reform.roleassignment.data.roleassignment.RoleAssignmentRepository;
 import uk.gov.hmcts.reform.roleassignment.domain.model.RequestedRole;
 import uk.gov.hmcts.reform.roleassignment.domain.model.RoleAssignmentRequest;
-import uk.gov.hmcts.reform.roleassignment.domain.model.enums.Status;
 import uk.gov.hmcts.reform.roleassignment.util.JacksonUtils;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 
 @Service
 public class PersistenceService {
-    //1. StoreRequest,
-    //2. StoreAssignment
+    //1. StoreRequest which will insert records in request and history table with log,
+    //2. Insert new Assignment record with updated Status in historyTable
     //3. Update Request Status
-    //4. Update Assignment Status
-    //5. Make Assignment to Live
-    private HistoryRepository historyRepository;
-    private HistoryStatusRepository historyStatusRepository;
-    private RequestRepository requestRepository;
-    private RequestStatusRepository requestStatusRepository;
-    private RoleAssignmentRepository roleAssignmentRepository;
+    //4. Store live record in the roleAssignmentTable
+    private HistoryRepository roleAssignmentRepository;
 
-    public PersistenceService(HistoryRepository historyRepository, HistoryStatusRepository historyStatusRepository,
-                              RequestRepository requestRepository, RequestStatusRepository requestStatusRepository,
-                              RoleAssignmentRepository roleAssignmentRepository) {
-        this.historyRepository = historyRepository;
-        this.historyStatusRepository = historyStatusRepository;
-        this.requestRepository = requestRepository;
+    public PersistenceService(HistoryRepository roleAssignmentRepository) {
         this.roleAssignmentRepository = roleAssignmentRepository;
     }
 
@@ -46,10 +29,9 @@ public class PersistenceService {
         ArrayList<HistoryEntity> historyEntityList = new ArrayList<>();
         for (RequestedRole roleAssignment : roleAssignmentRequest.requestedRoles) {
             HistoryEntity historyEntity = convertModelToEntity(roleAssignment);
-            buildRoleAssignmentHistoryStatus(historyEntity);
             historyEntityList.add(historyEntity);
         }
-        historyRepository.saveAll(historyEntityList);
+        roleAssignmentRepository.saveAll(historyEntityList);
 
     }
 
@@ -66,18 +48,7 @@ public class PersistenceService {
             .status(model.getStatus().toString())
             .readOnly(Boolean.TRUE)
             .build();
-        historyEntity.setRoleAssignmentHistoryStatusEntities(new HashSet<HistoryStatusEntity>());
         return historyEntity;
-    }
-
-    private void buildRoleAssignmentHistoryStatus(HistoryEntity historyEntity) {
-        HistoryStatusEntity historyStatusEntity = HistoryStatusEntity.builder().historyEntity(
-            historyEntity)
-            .log("professional drools rule")
-            .status(Status.CREATED.toString())
-            .sequence(102)
-            .build();
-        historyEntity.getRoleAssignmentHistoryStatusEntities().add(historyStatusEntity);
     }
 
 }
