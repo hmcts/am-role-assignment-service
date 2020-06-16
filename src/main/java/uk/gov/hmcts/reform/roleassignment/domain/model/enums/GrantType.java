3fc1925bdc257f7b88079a15410dc8467ee99ae3
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-06-16 10:00:44 +0100

    Latest changes as per the LLD updates.

--- src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/enums/GrantType.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/enums/GrantType.java
@@ -1,5 +1,5 @@
 package uk.gov.hmcts.reform.roleassignment.domain.model.enums;
 
 public enum GrantType {
-        SPECIFIC, STANDARD, CHALLENGED, SEARCH
+    BASIC,SPECIFIC, STANDARD, CHALLENGED, EXCLUDED
 }
