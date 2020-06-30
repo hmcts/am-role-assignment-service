d178b752919d6f43c9639c08bfd0f7759e5015c1
ConorH994 <conor.halpin@hmcts.net>
2020-06-30 14:40:46 +0100

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/roleassignment/controller/endpoints/GetAssignmentController.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/controller/endpoints/GetAssignmentController.java
@@ -33,8 +33,6 @@ import uk.gov.hmcts.reform.roleassignment.v1.V1;
 import static uk.gov.hmcts.reform.roleassignment.apihelper.Constants.ROLES_JSON;
 import static uk.gov.hmcts.reform.roleassignment.apihelper.Constants.APPLICATION_JSON;
 
-import static org.springframework.http.HttpStatus.UNSUPPORTED_MEDIA_TYPE;
-
 import java.io.InputStream;
 import java.text.ParseException;
 import java.util.ArrayList;
