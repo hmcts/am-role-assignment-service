8722bf5b316ee50d3e6982e1f0c6aac453895da1
dharmendra8186 <dharmendra.singh@hmcts.net>
2020-07-03 09:52:14 +0100

    AM-841 : junit for delete api.

--- src/test/java/uk/gov/hmcts/reform/roleassignment/domain/service/deleteroles/DeleteRoleAssignmentOrchestratorTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/domain/service/deleteroles/DeleteRoleAssignmentOrchestratorTest.java
@@ -1,7 +1,39 @@
 package uk.gov.hmcts.reform.roleassignment.domain.service.deleteroles;
 
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.roleassignment.domain.service.common.ParseRequestService;
+import uk.gov.hmcts.reform.roleassignment.domain.service.common.PersistenceService;
+import uk.gov.hmcts.reform.roleassignment.domain.service.common.ValidationModelService;
 
+import static org.mockito.Mockito.mock;
+
+@RunWith(MockitoJUnitRunner.class)
 class DeleteRoleAssignmentOrchestratorTest {
+
+    @Mock
+    private ParseRequestService parseRequestService = mock(ParseRequestService.class);
+    @Mock
+    private PersistenceService persistenceService = mock(PersistenceService.class);
+    @Mock
+    private ValidationModelService validationModelService = mock(ValidationModelService.class);
+
+
+    @InjectMocks
+    private DeleteRoleAssignmentOrchestrator sut = new DeleteRoleAssignmentOrchestrator(
+        persistenceService,
+        parseRequestService,
+        validationModelService
+    );
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
 }
