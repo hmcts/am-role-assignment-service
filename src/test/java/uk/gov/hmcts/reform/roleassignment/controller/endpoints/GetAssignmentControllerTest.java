5e259c3e41770ba2fc0f11c8ca61094496369706
Nitin <nitin.sahu@hmcts.net>
2020-07-03 15:59:47 +0100

    AM-847: JUnit Test Get role assignments

--- src/test/java/uk/gov/hmcts/reform/roleassignment/controller/endpoints/GetAssignmentControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/controller/endpoints/GetAssignmentControllerTest.java
@@ -5,11 +5,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import uk.gov.hmcts.reform.roleassignment.domain.service.common.ParseRequestService;
+import uk.gov.hmcts.reform.roleassignment.domain.service.common.PersistenceService;
+import uk.gov.hmcts.reform.roleassignment.domain.service.createroles.CreateRoleAssignmentOrchestrator;
+import uk.gov.hmcts.reform.roleassignment.domain.service.getroles.RetrieveRoleAssignmentOrchestrator;
+import uk.gov.hmcts.reform.roleassignment.feignclients.DataStoreFeignClient;
+import uk.gov.hmcts.reform.roleassignment.helper.TestDataBuilder;
+import static org.mockito.Mockito.doReturn;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.gov.hmcts.reform.roleassignment.apihelper.Constants.APPLICATION_JSON;
@@ -19,8 +27,28 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @RunWith(MockitoJUnitRunner.class)
 class GetAssignmentControllerTest {
 
+    @Mock
+    private transient PersistenceService persistenceServiceMock;
+
+    @Mock
+    private transient DataStoreFeignClient dataStoreFeignClientMock;
+
+    @Mock
+    private transient RetrieveRoleAssignmentOrchestrator retrieveRoleAssignmentServiceMock;
+
+    @Mock
+    private transient ParseRequestService parseRequestService;
+
+    @Mock
+    private CreateRoleAssignmentOrchestrator createRoleAssignmentService;
+
+
     @InjectMocks
-    private GetAssignmentController sut;
+    private GetAssignmentController sut = new GetAssignmentController(parseRequestService,
+                                                                      persistenceServiceMock,
+                                                                      dataStoreFeignClientMock,
+                                                                      createRoleAssignmentService,
+                                                                      retrieveRoleAssignmentServiceMock);
 
     @BeforeEach
     public void setUp() {
@@ -39,5 +67,16 @@ class GetAssignmentControllerTest {
         Assertions.assertThrows(HttpMediaTypeNotAcceptableException.class, () -> {
             ResponseEntity<Object> response = sut.getListOfRoles("application/pdf");
         });
+    }
+
+    @Test
+    void shouldGetRoleAssignment() throws Exception {
+        String actorId = "123e4567-e89b-42d3-a456-556642445678";
+        ResponseEntity<Object> expectedResponse = TestDataBuilder.buildRoleAssignmentResponse();
+        doReturn(expectedResponse).when(retrieveRoleAssignmentServiceMock).getAssignmentsByActor(actorId);
+        ResponseEntity<Object> response = sut.retrieveRoleAssignmentsByActorId(actorId);
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(expectedResponse.getBody(), response.getBody());
     }
 }
