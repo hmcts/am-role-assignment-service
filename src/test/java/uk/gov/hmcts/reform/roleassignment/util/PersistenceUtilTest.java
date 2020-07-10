3bbcc83a645d7a4ffb38d47a539db13c870c1904
ConorH994 <conor.halpin@hmcts.net>
2020-07-10 16:05:51 +0100

    started persistence testing

---
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/util/PersistenceUtilTest.java
@@ -0,0 +1,54 @@
+package uk.gov.hmcts.reform.roleassignment.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.roleassignment.domain.service.security.IdamRoleService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(MockitoJUnitRunner.class)
+class PersistenceUtilTest {
+
+    @InjectMocks
+    PersistenceUtil idamRoleService = new PersistenceUtil();
+
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void convertRoleAssignmentToHistoryEntity() {
+        //idamRoleService.convertRoleAssignmentToHistoryEntity();
+    }
+
+    @Test
+    void convertRequestToEntity() {
+        //idamRoleService.convertRequestToEntity();
+    }
+
+    @Test
+    void convertRoleAssignmentToEntity() {
+        //idamRoleService.convertRoleAssignmentToEntity();
+    }
+
+    @Test
+    void convertActorCacheToEntity() {
+        //idamRoleService.convertActorCacheToEntity();
+    }
+
+    @Test
+    void convertHistoryEntityToRoleAssignment() {
+        //idamRoleService.convertHistoryEntityToRoleAssignment();
+    }
+
+    @Test
+    void convertEntityToRoleAssignment() {
+        //idamRoleService.convertEntityToRoleAssignment();
+    }
+}
