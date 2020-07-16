c22fdbe902f3fec71086771fce0dfc88b07e9f9a
Nitin <nitin.sahu@hmcts.net>
2020-07-16 10:14:32 +0100

    duplicate block issue fix

--- src/main/java/uk/gov/hmcts/reform/assignment/data/HistoryEntity.java
+++ src/main/java/uk/gov/hmcts/reform/assignment/data/HistoryEntity.java
@@ -38,14 +38,26 @@ public class HistoryEntity {
     @Column(name = "actor_id_type", nullable = false)
     private String actorIdType;
 
+    @Column(name = "process")
+    private String process;
+
     @Column(name = "actor_id", nullable = false)
     private UUID actorId;
+
+    @Column(name = "reference")
+    private String reference;
 
     @Column(name = "role_type", nullable = false)
     private String roleType;
 
+    @Column(name = "log")
+    private String log;
+
     @Column(name = "role_name", nullable = false)
     private String roleName;
+
+    @Column(name = "status_sequence", nullable = false)
+    private int sequence;
 
     @Column(name = "classification", nullable = false)
     private String classification;
@@ -53,41 +65,29 @@ public class HistoryEntity {
     @Column(name = "grant_type", nullable = false)
     private String grantType;
 
+    @Column(name = "notes", nullable = true, columnDefinition = "jsonb")
+    @Convert(converter = JsonBConverter.class)
+    private JsonNode notes;
+
     @Column(name = "role_category")
     private String roleCategory;
 
     @Column(name = "read_only", nullable = false)
     private boolean readOnly;
 
-    @Column(name = "begin_time")
-    private LocalDateTime beginTime;
-
-    @Column(name = "end_time")
-    private LocalDateTime endTime;
-
-    @Column(name = "process")
-    private String process;
-
-    @Column(name = "reference")
-    private String reference;
-
-    @Column(name = "log")
-    private String log;
-
-    @Column(name = "status_sequence", nullable = false)
-    private int sequence;
-
     @CreationTimestamp
     @Column(name = "created", nullable = false)
     private LocalDateTime created;
+
+    @Column(name = "begin_time")
+    private LocalDateTime beginTime;
 
     @Column(name = "attributes", nullable = false, columnDefinition = "jsonb")
     @Convert(converter = JsonBConverter.class)
     private JsonNode attributes;
 
-    @Column(name = "notes", nullable = true, columnDefinition = "jsonb")
-    @Convert(converter = JsonBConverter.class)
-    private JsonNode notes;
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
 
     @Id
     @ManyToOne(fetch = FetchType.LAZY)
