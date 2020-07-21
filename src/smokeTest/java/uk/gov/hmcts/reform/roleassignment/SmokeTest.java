46648151989c5014367cc86f942c2f1b19be09f0
SivaKanukolluQA <siva.kanukollu@hmcts.net>
2020-07-21 14:56:43 +0100

    AM-885: Smoke Tests

--- src/smokeTest/java/uk/gov/hmcts/reform/roleassignment/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/roleassignment/SmokeTest.java
@@ -1,27 +1,35 @@
 package uk.gov.hmcts.reform.roleassignment;
 
-import org.junit.Assert;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
+
+@SpringBootTest
+@RunWith(SpringIntegrationSerenityRunner.class)
+//@ConfigurationProperties()
 public class SmokeTest {
-
     @Value("${roleAssignmentUrl}")
-    private String baseUrl;
-
-    /*@Test
-    public void shouldGetHealthStatus() {
-        RestAssured.baseURI = baseUrl;
+    String roleAssignmentUrl;
+    @Test
+    public void sample_test_setup_should_receive_response_for_role_assignment_api() {
+        RestAssured.baseURI = roleAssignmentUrl;
         RestAssured.useRelaxedHTTPSValidation();
         Response response = SerenityRest
             .given()
             .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
             .when()
-            .get("/health")
+            .get("/")
             .andReturn();
         response.then().assertThat().statusCode(HttpStatus.OK.value());
-    }*/
-
-    public void sampleTest() {
-        Assert.assertTrue(1 == 1);
     }
 }
