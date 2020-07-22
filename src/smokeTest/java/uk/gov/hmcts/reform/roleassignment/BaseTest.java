ea79a1491cb7b8659a77819f701bcb18430c370a
Nitish Singla <nitish.singla@hmcts.net>
2020-07-22 15:37:55 +0100

    AM-918: Added S2S token generation.

--- src/smokeTest/java/uk/gov/hmcts/reform/roleassignment/BaseTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/roleassignment/BaseTest.java
@@ -2,17 +2,42 @@ package uk.gov.hmcts.reform.roleassignment;
 
 import feign.Feign;
 import feign.jackson.JacksonEncoder;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
+import uk.gov.hmcts.reform.idam.client.IdamApi;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.idam.client.models.TokenRequest;
+import uk.gov.hmcts.reform.idam.client.models.TokenResponse;
+import uk.gov.hmcts.reform.idam.client.models.UserDetails;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
 @Service
+@NoArgsConstructor
 public class BaseTest {
-    private static final String AUTHORIZATION_CODE = "authorization_code";
-    private static final String CODE = "code";
-    private static final String BASIC = "Basic ";
+
+    @Autowired
+    private IdamClient idamClient;
+    @Autowired
+    private IdamApi idamApi;
+
+    @Value("${client.id}")
+    private String clientId;
+    @Value("${client.secret}")
+    private String clientSecret;
+    @Value("${client.redirectUri}")
+    private String redirectUri;
+
+    @Value("${user.username}")
+    private String username;
+    @Value("${user.password}")
+    private String password;
+    @Value("${user.scope}")
+    private  String scope;
 
     public ServiceAuthorisationApi generateServiceAuthorisationApi(final String s2sUrl) {
         return Feign.builder()
@@ -21,11 +46,34 @@ public class BaseTest {
                     .target(ServiceAuthorisationApi.class, s2sUrl);
     }
 
-
     public ServiceAuthTokenGenerator authTokenGenerator(
-        @Value("${idam.s2s-auth.totp_secret}") final String secret,
-        @Value("${idam.s2s-auth.microservice}") final String microService,
+        final String secret,
+        final String microService,
         final ServiceAuthorisationApi serviceAuthorisationApi) {
         return new ServiceAuthTokenGenerator(secret, microService, serviceAuthorisationApi);
+    }
+
+    public UserInfo getUserInfo(String jwtToken) {
+        return idamClient.getUserInfo("Bearer " + jwtToken);
+    }
+
+    public UserDetails getUserByUserId(String jwtToken, String userId) {
+        return idamClient.getUserByUserId("Bearer " + jwtToken, userId);
+    }
+
+    public String getManageUserToken() {
+        TokenRequest tokenRequest = new TokenRequest(
+            clientId,
+            clientSecret,
+            "password",
+            redirectUri,
+            username,
+            password,
+            scope,
+            "4",
+            ""
+        );
+        TokenResponse tokenResponse = idamApi.generateOpenIdToken(tokenRequest);
+        return tokenResponse.accessToken;
     }
 }
--- src/smokeTest/java/uk/gov/hmcts/reform/roleassignment/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/roleassignment/SmokeTest.java
@@ -5,19 +5,23 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lombok.NoArgsConstructor;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@RunWith(SpringIntegrationSerenityRunner.class)
-//@ConfigurationProperties()
-public class SmokeTest extends BaseTest {
+@RunWith(SpringRunner.class)
+@NoArgsConstructor
+@TestPropertySource(value = "classpath:smokeTest.yaml")
+public class SmokeTest {
     @Value("${roleAssignmentUrl}")
     String roleAssignmentUrl;
 
@@ -27,6 +31,9 @@ public class SmokeTest extends BaseTest {
     String microService;
     @Value("${idam.s2s-auth.url}")
     String s2sUrl;
+
+    @Autowired
+    BaseTest baseTest;
 
     @Test
     public void sample_test_setup_should_receive_response_for_role_assignment_api() {
@@ -46,7 +53,7 @@ public class SmokeTest extends BaseTest {
     public void should_receive_response_for_a_get_document_meta_data() {
 
         String serviceAuth = new BaseTest()
-            .authTokenGenerator(secret, microService, generateServiceAuthorisationApi(s2sUrl)).generate();
+            .authTokenGenerator(secret, microService, baseTest.generateServiceAuthorisationApi(s2sUrl)).generate();
         String targetInstance = roleAssignmentUrl + "/am/role-assignments?roleType=case&caseId=1234567890000000";
 
         RestAssured.baseURI = targetInstance;
@@ -56,11 +63,7 @@ public class SmokeTest extends BaseTest {
             .given()
             .relaxedHTTPSValidation()
             .header("ServiceAuthorization", "Bearer " + serviceAuth)
-            .header(
-                "Authorization",
-                "Bearer "
-                +
-                "eyJ0eXAiOiJKV1QiLCJ6aXAiOiJOT05FIiwia2lkIjoiYi9PNk92VnYxK3krV2dySDVVaTlXVGlvTHQwPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJhbS5kb2NrZXIuZGVmYXVsdEBobWN0cy5uZXQiLCJhdXRoX2xldmVsIjowLCJhdWRpdFRyYWNraW5nSWQiOiJiYWE1MGYwOC1kMjk5LTQxNTgtYTk1Ny0zYjAyNDZmZmE2ZGQiLCJpc3MiOiJodHRwOi8vZnItYW06ODA4MC9vcGVuYW0vb2F1dGgyL2htY3RzIiwidG9rZW5OYW1lIjoiYWNjZXNzX3Rva2VuIiwidG9rZW5fdHlwZSI6IkJlYXJlciIsImF1dGhHcmFudElkIjoiZDJiNTFlZDMtZTg1Ni00OTZhLTliYjAtMjhkZmQ5MmUyMDYxIiwiYXVkIjoiYW1fZG9ja2VyIiwibmJmIjoxNTk1NDA5MzY0LCJncmFudF90eXBlIjoicGFzc3dvcmQiLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwicm9sZXMiLCJhdXRob3JpdGllcyJdLCJhdXRoX3RpbWUiOjE1OTU0MDkzNjQsInJlYWxtIjoiL2htY3RzIiwiZXhwIjoxNTk1NDM4MTY0LCJpYXQiOjE1OTU0MDkzNjQsImV4cGlyZXNfaW4iOjI4ODAwLCJqdGkiOiI4MTc1ZDJjNy0yODgxLTQzZjktOWZhZC1lNTQ5ZTZjMDhjNzUifQ.n53QPbw60N3RB76a-yJjG4j6p28jiamX7wj7WJPlLVmPYhMssQLLOW4-ddsZe3jfgm-nhzYjd6teb6bChwXFKCkQFJaEO9ZfE72nWQH934UBiDxD9UMLeglxySlNoKoCDWrBO2lQcuaMCWSlKrQHxBywG_Ubj3Gee3LheXloUN5kWgQyPlYEsQisWCB4uUzfqle1DdyZjOeSxnmAxK19tbpcQwb65YjRe1nsaphQAhcI3TWBtlUSyYth-RlumbIPuCowWhDULH6ekLxeqFz3x9_UkK2qYi8o5J1zsOZZKhg6Voy6Bt_uCwZ5wE2modbkt98MKB18aS2UuZGtAnviAg")
+            .header("Authorization", "Bearer " + baseTest.getManageUserToken())
             .when()
             .get("am/role-assignments?roleType=case&actorId=123e4567-e89b-42d3-a456-556642445612")
             .andReturn();
