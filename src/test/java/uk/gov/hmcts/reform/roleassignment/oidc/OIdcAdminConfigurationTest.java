e4b773c7324262a8e13b20b056b5ad3651f07875
ConorH994 <conor.halpin@hmcts.net>
2020-07-24 17:10:21 +0100

    checkstyle

--- src/test/java/uk/gov/hmcts/reform/roleassignment/oidc/OIdcAdminConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/oidc/OIdcAdminConfigurationTest.java
@@ -2,7 +2,7 @@ package uk.gov.hmcts.reform.roleassignment.oidc;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class OIdcAdminConfigurationTest {
 
