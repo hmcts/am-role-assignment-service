a08d379421807726a4e30d02b0ca93835b10a171
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-07-29 10:47:49 +0100

    Style issue fix

--- src/test/java/uk/gov/hmcts/reform/roleassignment/oidc/IdamRepositoryTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/oidc/IdamRepositoryTest.java
@@ -11,16 +11,11 @@ import uk.gov.hmcts.reform.idam.client.IdamApi;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.idam.client.OAuth2Configuration;
 import uk.gov.hmcts.reform.idam.client.models.TokenResponse;
-import uk.gov.hmcts.reform.idam.client.models.UserDetails;
-import uk.gov.hmcts.reform.idam.client.models.UserInfo;
-import uk.gov.hmcts.reform.roleassignment.helper.TestDataBuilder;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-import java.io.IOException;
 
 @RunWith(MockitoJUnitRunner.class)
 class IdamRepositoryTest {
