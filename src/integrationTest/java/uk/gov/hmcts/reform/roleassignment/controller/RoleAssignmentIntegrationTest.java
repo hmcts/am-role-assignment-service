fef7b812f91c469f06e1cbf1b888df3891477b8e
Nitin <nitin.sahu@hmcts.net>
2020-08-05 09:57:39 +0100

    AM-934:Integration test for get end points.

--- src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/controller/RoleAssignmentIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/controller/RoleAssignmentIntegrationTest.java
@@ -1,116 +1,54 @@
 package uk.gov.hmcts.reform.roleassignment.controller;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
-import uk.gov.hmcts.reform.idam.client.models.TokenRequest;
-import uk.gov.hmcts.reform.idam.client.models.TokenResponse;
-import uk.gov.hmcts.reform.roleassignment.controller.advice.exception.BadRequestException;
-import uk.gov.hmcts.reform.roleassignment.controller.advice.exception.ResourceNotFoundException;
-import uk.gov.hmcts.reform.roleassignment.controller.endpoints.GetAssignmentController;
-import uk.gov.hmcts.reform.roleassignment.controller.endpoints.QueryAssignmentController;
 import uk.gov.hmcts.reform.roleassignment.domain.model.RoleAssignment;
 import uk.gov.hmcts.reform.roleassignment.domain.model.RoleAssignmentResource;
-import uk.gov.hmcts.reform.roleassignment.domain.service.common.PersistenceService;
-import uk.gov.hmcts.reform.roleassignment.domain.service.common.PrepareResponseService;
-import uk.gov.hmcts.reform.roleassignment.domain.service.getroles.RetrieveRoleAssignmentOrchestrator;
-import uk.gov.hmcts.reform.roleassignment.domain.service.queryroles.QueryRoleAssignmentOrchestrator;
-import uk.gov.hmcts.reform.roleassignment.util.SecurityUtils;
 
 import javax.inject.Inject;
 import javax.sql.DataSource;
-
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class RoleAssignmentIntegrationTest extends BaseTest {
 
     private static final Logger logger = LoggerFactory.getLogger(RoleAssignmentIntegrationTest.class);
     private static final String COUNT_HISTORY_RECORDS_QUERY = "SELECT count(1) as n FROM role_assignment_history";
-    private static final String COUNT_ASSIGNMENT_RECORDS_QUERY = "SELECT count(1) as n FROM role_assignment";
 
     private static final String GET_ASSIGNMENT_STATUS_QUERY = "SELECT actor_id FROM role_assignment where id = ?";
+
     private transient MockMvc mockMvc;
 
     private JdbcTemplate template;
 
-    RestTemplate restTemplate = new RestTemplate();
-
     @Inject
     private WebApplicationContext wac;
-
-    @Mock
-    private Authentication authentication;
-
-    @InjectMocks
-    private GetAssignmentController getAssignmentController;
-
-    @Mock
-    private SecurityContext securityContext;
-
-    @Mock
-    private PersistenceService persistenceService = mock(PersistenceService.class);
-
-    @Mock
-    private PrepareResponseService prepareResponseService = mock(PrepareResponseService.class);
-
-    @Mock
-    private SecurityUtils securityUtils;
 
     @Inject
     protected DataSource db;
 
-    private final QueryRoleAssignmentOrchestrator queryRoleAssignmentOrchestrator =
-        mock(QueryRoleAssignmentOrchestrator.class);
-
-    @InjectMocks
-    private final QueryAssignmentController sut = new QueryAssignmentController(queryRoleAssignmentOrchestrator);
-
-    RetrieveRoleAssignmentOrchestrator retrieveRoleAssignmentOrchestrator = new RetrieveRoleAssignmentOrchestrator(
-        persistenceService,
-        prepareResponseService
-    );
-
     @Before
     public void setUp() {
         template = new JdbcTemplate(db);
-        //mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-        //mockMvc = MockMvcBuilders.standaloneSetup(sut).build();
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
         MockitoAnnotations.initMocks(this);
-
-        doReturn(authentication).when(securityContext).getAuthentication();
-
-        SecurityContextHolder.setContext(securityContext);
-        getAssignmentController = new GetAssignmentController(retrieveRoleAssignmentOrchestrator);
-
-        //MockUtils.setSecurityAuthorities(UID_NO_EVENT_ACCESS, authentication, MockUtils.ROLE_CASEWORKER_PUBLIC);
-
     }
 
     @Test
@@ -118,7 +56,7 @@ public class RoleAssignmentIntegrationTest extends BaseTest {
         "classpath:sql/insert_role_assignment_request.sql",
         "classpath:sql/insert_role_assignment_history.sql"
     })
-    public void shoudGetRecordCountFromHistoryTable() throws Exception {
+    public void shouldGetRecordCountFromHistoryTable() throws Exception {
         final int count = template.queryForObject(COUNT_HISTORY_RECORDS_QUERY, Integer.class);
         logger.info(" Total number of records fetched from role assignment history table...{}", count);
         assertEquals(
@@ -127,7 +65,7 @@ public class RoleAssignmentIntegrationTest extends BaseTest {
 
     @Test
     @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:sql/insert_role_assignment.sql"})
-    public void shoudGetRecordsFromRoleAssignmentTable() throws Exception {
+    public void shouldGetRecordsFromRoleAssignmentTable() throws Exception {
         final Object[] parameters = new Object[]{
             "638e8e7a-7d7c-4027-9d53-ea4b1095eab1"
         };
@@ -151,7 +89,6 @@ public class RoleAssignmentIntegrationTest extends BaseTest {
             .andExpect(status().is(200))
             .andReturn();
 
-        logger.info(" result...{}", result);
         RoleAssignmentResource response = mapper.readValue(
             result.getResponse().getContentAsString(),
             RoleAssignmentResource.class
@@ -176,6 +113,8 @@ public class RoleAssignmentIntegrationTest extends BaseTest {
         final MvcResult result = mockMvc.perform(get(url)
                                                      .contentType(MediaType.APPLICATION_JSON)
                                                      .headers(getHttpHeaders())
+                                                     .param("roleType", "case")
+                                                     .param("actorId", "123e4567-e89b-42d3-a456-556642445612")
         )
             .andExpect(status().is(200))
             .andReturn();
@@ -183,15 +122,77 @@ public class RoleAssignmentIntegrationTest extends BaseTest {
 
         List<RoleAssignment> response = Arrays.asList(mapper.readValue(responseAsString, RoleAssignment[].class));
         String contentAsString = result.getResponse().getContentAsString();
-
+        assertEquals(200, result.getResponse().getStatus());
+        assertEquals(
+            "2ef8ebf3-266e-45d3-a3b8-4ce1e5d93b9f",
+            response.get(0).getId().toString()
+        );
+        assertEquals(
+            "123e4567-e89b-42d3-a456-556642445612",
+            response.get(0).getActorId().toString()
+        );
     }
 
-    @NotNull
-    private HttpHeaders getHttpHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("ServiceAuthorization", "Bearer " + "1234");
-        headers.add("Authorization", "Bearer " + "2345");
-        return headers;
+
+    @Test
+    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:sql/insert_role_assignment.sql"})
+    public void shouldGetRoleAssignmentsBasedOnRoleTypeAndCaseId() throws Exception {
+        assertRoleAssignmentRecordSize();
+        final String url = "/am/role-assignments";
+
+        final MvcResult result = mockMvc.perform(get(url)
+                                                     .contentType(MediaType.APPLICATION_JSON)
+                                                     .headers(getHttpHeaders())
+                                                     .param("roleType", "case")
+                                                     .param("caseId", "1234567890123456")
+        )
+            .andExpect(status().is(200))
+            .andReturn();
+        String responseAsString = result.getResponse().getContentAsString();
+
+        List<RoleAssignment> response = Arrays.asList(mapper.readValue(responseAsString, RoleAssignment[].class));
+        String contentAsString = result.getResponse().getContentAsString();
+        assertEquals(200, result.getResponse().getStatus());
+        assertEquals(
+            "2ef8ebf3-266e-45d3-a3b8-4ce1e5d93b9f",
+            response.get(0).getId().toString()
+        );
+        assertEquals(
+            "123e4567-e89b-42d3-a456-556642445612",
+            response.get(0).getActorId().toString()
+        );
+    }
+
+    @Test
+    public void shouldGetListOfRoles() throws Exception {
+        final String url = "/am/role-assignments/roles";
+
+        final MvcResult result = mockMvc.perform(get(url)
+                                                     .contentType(MediaType.APPLICATION_JSON)
+                                                     .headers(getHttpHeaders())
+        )
+            .andExpect(status().is(200))
+            .andReturn();
+        String response = result.getResponse().getContentAsString();
+
+        JsonNode jsonResonse = mapper.readValue(response, JsonNode.class);
+        assertEquals(200, result.getResponse().getStatus());
+        assertEquals(
+            2,
+            jsonResonse.size()
+        );
+        assertEquals(
+            "judge",
+            jsonResonse.get(0).get("name").asText()
+        );
+        assertEquals(
+            "Judicial office holder able to do judicial case work",
+            jsonResonse.get(0).get("description").asText()
+        );
+        assertEquals(
+            "JUDICIAL",
+            jsonResonse.get(0).get("category").asText()
+        );
     }
 
     private void assertRoleAssignmentRecordSize() {
@@ -204,69 +205,11 @@ public class RoleAssignmentIntegrationTest extends BaseTest {
             "Role assignment actor Id", "123e4567-e89b-42d3-a456-556642445612", actorId);
     }
 
-    /*@Test
-    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:sql/sample.sql" })
-    public void shouldDeleteRoleAssignments() throws Exception {
-        //doReturn("serviceName").when(securityUtils).getServiceName();
-        //doReturn("638e8e7a-7d7c-4027-9d53-ea4b1095eab1").when(securityUtils).getUserId();
-        final String url = "/am/role-assignments";
-        String userToken = searchUserByUserId();
-        logger.info(userToken);
+    @NotNull
+    private HttpHeaders getHttpHeaders() {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("ServiceAuthorization", "Bearer " + "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjY2RfZ3ciLCJleHAiOjE1OTYyMjIwNDZ9.6dyTWiXaaSjfOZpqg9ieHakQARgG5rdlJjfoolI70RJD4V0gRFMFIJ6ULF3c-SBoQAZk0rhlixf7Id74DA7iBQ");
-        headers.add("Authorization", userToken);
-
-        final MvcResult result = mockMvc.perform(delete(url)
-                                                     .contentType(MediaType.APPLICATION_JSON)
-                                                     .headers(headers)
-                                                     .param("process", "S-052")
-                                                     .param("reference", "S-052")
-        )
-            .andExpect(status().is(200))
-            .andReturn();
-        String contentAsString = result.getResponse().getContentAsString();
-        logger.info(" contentAsString...{}", contentAsString);
-
+        headers.add("ServiceAuthorization", "Bearer " + "1234");
+        headers.add("Authorization", "Bearer " + "2345");
+        return headers;
     }
-
-
-    public String searchUserByUserId() {
-
-        ResponseEntity<TokenResponse> response = new ResponseEntity<>(HttpStatus.OK);
-        HttpHeaders headers = new HttpHeaders();
-        try {
-            String url = String.format(
-                "%s/o/token?client_id=%s&client_secret=%s&grant_type=%s&scope=%s&username=%s&password=%s",
-                "http://localhost:5000",
-                "am_docker",
-                "am_docker_secret",
-                "password",
-                "openid+profile+roles+authorities",
-                "befta.caseworker.2.solicitor.2@gmail.com",
-                "Pa55word11"
-            );
-            logger.info("URL :   {}", url);
-            headers.setContentType(MediaType.parseMediaType(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
-            HttpEntity<?> entity = new HttpEntity<>(headers);
-            response = restTemplate.exchange(
-                url,
-                HttpMethod.POST,
-                entity,
-                TokenResponse.class
-            );
-
-            if (HttpStatus.OK.equals(response.getStatusCode())) {
-                logger.info("Positive response");
-                return response.getBody().accessToken;
-            } else {
-                logger.error("There is some problem in fetching access token {}", response
-                    .getStatusCode());
-                throw new ResourceNotFoundException("Not Found");
-            }
-        } catch (HttpClientErrorException exception) {
-            logger.error("HttpClientErrorException {}", exception.getMessage());
-            throw new BadRequestException("Unable to fetch access token");
-
-        }
-    }*/
 }
