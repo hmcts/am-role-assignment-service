4dfb5ee7016542c21d1ea23cde03b6b023f304de
Nitish Singla <nitish.singla@hmcts.net>
2020-08-17 15:21:29 +0100

    AM-944: Marking tests as excluded.

--- src/main/java/uk/gov/hmcts/reform/roleassignment/util/EnvironmentVariableUtils.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/util/EnvironmentVariableUtils.java
@@ -3,16 +3,6 @@ package uk.gov.hmcts.reform.roleassignment.util;
 import org.apache.commons.lang3.Validate;
 
 public class EnvironmentVariableUtils {
-
-    public static String resolvePossibleVariable(String key) {
-        if (key.startsWith("[[$")) {
-            String envKey = key.substring(3, key.length() - 2);
-            String envValue = getRequiredVariable(envKey);
-            return envValue;
-        }
-        return key;
-    }
-
     private EnvironmentVariableUtils(){
 
     }
@@ -20,9 +10,4 @@ public class EnvironmentVariableUtils {
     public static String getRequiredVariable(String name) {
         return Validate.notNull(System.getenv(name), "Environment variable `%s` is required", name);
     }
-
-    public static String getOptionalVariable(String name) {
-        return System.getenv(name);
-    }
-
 }
