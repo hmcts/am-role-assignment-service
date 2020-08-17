0597f2e88c7a3a9b76e4fe3b1c7fbb1fee52e40b
Nitish Singla <nitish.singla@hmcts.net>
2020-08-17 16:09:18 +0100

    AM-944: Adding Junit test.

--- src/test/java/uk/gov/hmcts/reform/roleassignment/util/EnvironmentVariableUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/util/EnvironmentVariableUtilsTest.java
@@ -1,13 +1,15 @@
 package uk.gov.hmcts.reform.roleassignment.util;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class EnvironmentVariableUtilsTest {
 
     @Test
     void getRequiredVariable() {
-        assertNotNull(EnvironmentVariableUtils.getRequiredVariable("TEST_URL"));
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            EnvironmentVariableUtils.getRequiredVariable("A_DUMMY_VARIABLE");
+        });
+
     }
 }
