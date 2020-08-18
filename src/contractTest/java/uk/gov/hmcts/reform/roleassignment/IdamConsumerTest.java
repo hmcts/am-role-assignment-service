06b9c5b3cee855b630fb4c673a48139122948725
ConorH994 <conor.halpin@hmcts.net>
2020-08-18 11:07:54 +0100

    working state

--- src/contractTest/java/uk/gov/hmcts/reform/roleassignment/IdamConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/roleassignment/IdamConsumerTest.java
@@ -33,65 +33,16 @@ public class IdamConsumerTest {
 
     private static final String IDAM_OPEN_ID_TOKEN_URL = "/o/token";
 
+    //        rolesArray[0] = "befta.caseworker.2.solicitor.2";
 
-//    @Pact(provider = "Idam_api", consumer = "am_role_assignment_service")
-//    public RequestResponsePact executeGetIdamAccessTokenAndGet200(PactDslWithProvider builder) throws JSONException {
-//        String[] rolesArray = new String[1];
-//        rolesArray[0] = "befta.caseworker.2.solicitor.2";
-//        Map<String, String> responseHeaders = Maps.newHashMap();
-//        responseHeaders.put("Content-Type", "application/json");
-//        Map<String, Object> params = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-//        params.put("email", "befta.caseworker.2.solicitor.2@gmail.com");
-//        params.put("password", "Password123");
-//        params.put("forename","Admin");
-//        params.put("surname", "Adams");
-//        params.put("roles", rolesArray);
-//        return builder
-//            .given("a user exists", params)
-//            .uponReceiving("Provider takes user/pwd and returns Access Token to AM "
-//                               + "- ROLE ASSIGNMENT API")
-//            .path(IDAM_OPEN_ID_TOKEN_URL)
-//            .method(HttpMethod.POST.toString())
-//            .body("redirect_uri=http%3A%2F%2Fwww.dummy-pact-service.com%2Fcallback&client_id=pact&grant_type="
-//                      + "password&username=befta.caseworker.2.solicitor.2%40gmail" +
-//                      ".com&password=Password123&client_secret=pactsecret&scope="
-//                      + "openid profile roles",
-//                  "application/x-www-form-urlencoded")
-//            .willRespondWith()
-//            .status(HttpStatus.OK.value())
-//            .headers(responseHeaders)
-//            .body(createAuthResponse())
-//            .toPact();
-//    }
-//
-//    @Test
-//    @PactTestFor(pactMethod = "executeGetIdamAccessTokenAndGet200")
-//    public void should_post_to_token_endpoint_and_receive_access_token_with_200_response(MockServer mockServer)
-//        throws JSONException {
-//        String actualResponseBody =
-//            SerenityRest
-//                .given()
-//                .contentType(ContentType.URLENC)
-//                .formParam("redirect_uri",
-//                           "http://www.dummy-pact-service.com/callback")
-//                .formParam("client_id", "pact")
-//                .formParam("grant_type", "password")
-//                .formParam("username", "befta.caseworker.2.solicitor.2@gmail.com")
-//                .formParam("password", "Password123")
-//                .formParam("client_secret", "pactsecret")
-//                .formParam("scope",
-//                           "openid profile roles")
-//                .post(mockServer.getUrl() + IDAM_OPEN_ID_TOKEN_URL)
-//                .then()
-//                .log().all().extract().asString();
-//        JSONObject response = new JSONObject(actualResponseBody);
-//        assertThat(response).isNotNull();
-//        assertThat(response.getString("access_token")).isNotBlank();
-//        assertThat(response.getString("token_type")).isEqualTo("Bearer");
-//        assertThat(response.getString("expires_in")).isNotBlank();
-//    }
-//
+    //        params.put("email", "befta.caseworker.2.solicitor.2@gmail.com");
+    //        params.put("password", "Password123");
 
+    //                      + "password&username=befta.caseworker.2.solicitor.2%40gmail"
+    //                      + ".com&password=Pa55word11&client_secret=pactsecret&scope="
+
+    //                .formParam("username", "befta.caseworker.2.solicitor.2@gmail.com")
+    //                .formParam("password", "Pa55word11")
 
     @Pact(provider = "Idam_api", consumer = "am_role_assignment_service")
     public RequestResponsePact executeGetIdamAccessTokenAndGet200(PactDslWithProvider builder) throws JSONException {
