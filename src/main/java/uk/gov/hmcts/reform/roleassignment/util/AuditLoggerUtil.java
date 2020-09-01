2ce0e89e04511fff0ade708135d8bdc30c89578b
Nitish Singla <nitish.singla@hmcts.net>
2020-09-01 15:29:14 +0100

    Fixing the SonarLint issues.

--- src/main/java/uk/gov/hmcts/reform/roleassignment/util/AuditLoggerUtil.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/util/AuditLoggerUtil.java
@@ -50,7 +50,7 @@ public class AuditLoggerUtil {
 
     public static Set<String> buildCaseIds(ResponseEntity<RoleAssignmentRequestResource> response) {
         Set<String> caseIds = new HashSet<>();
-        if (response.getBody() != null) {
+        if (response != null && response.getBody() != null) {
             response.getBody().getRoleAssignmentRequest().getRequestedRoles()
                 .stream().map(RoleAssignment::getAttributes).forEach(obj -> obj.forEach((key, value) -> {
                     if (key.equals("caseId")) {
