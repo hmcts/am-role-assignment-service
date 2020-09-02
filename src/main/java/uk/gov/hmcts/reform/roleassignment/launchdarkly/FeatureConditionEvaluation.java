ff1411b04c00ea1e71e55dd5392821996a30d2d3
Nitish Singla <nitish.singla@hmcts.net>
2020-09-02 14:30:29 +0100

    AM-992: Launch Darkly Fixes.

--- src/main/java/uk/gov/hmcts/reform/roleassignment/launchdarkly/FeatureConditionEvaluation.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/launchdarkly/FeatureConditionEvaluation.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import uk.gov.hmcts.reform.roleassignment.controller.advice.exception.ForbiddenException;
+import uk.gov.hmcts.reform.roleassignment.controller.advice.exception.ResourceNotFoundException;
 import uk.gov.hmcts.reform.roleassignment.util.SecurityUtils;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,10 +28,15 @@ public class FeatureConditionEvaluation implements HandlerInterceptor {
                              @NotNull HttpServletResponse response, @NotNull Object arg2) throws Exception {
 
         Map<String, String> launchDarklyUrlMap = featureToggleService.getLaunchDarklyMap();
+        String flagName = launchDarklyUrlMap.get(request.getRequestURI());
 
-        if (launchDarklyUrlMap.get(request.getRequestURI()) == null
-            || launchDarklyUrlMap.get(request.getRequestURI()).isEmpty()) {
+        if (flagName == null || flagName.isEmpty()) {
             throw new ForbiddenException("The endpoint is not configured in Launch Darkly");
+        }
+
+        if (!featureToggleService.isValidFlag(flagName)) {
+            throw new ResourceNotFoundException(String.format(
+                "The flag %s is not configured in Launch Darkly", flagName));
         }
 
         boolean flagStatus = featureToggleService.isFlagEnabled(securityUtils.getServiceName(),
