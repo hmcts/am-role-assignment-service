f9426cea812a86c9959ec032e4c294d0db418f74
Nitish Singla <nitish.singla@hmcts.net>
2020-09-16 14:17:05 +0100

    AM-1010: Enabling the full Sonar.

--- src/test/java/uk/gov/hmcts/reform/roleassignment/launchdarkly/FeatureToggleServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/launchdarkly/FeatureToggleServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import uk.gov.hmcts.reform.roleassignment.config.FeatureToggleService;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
