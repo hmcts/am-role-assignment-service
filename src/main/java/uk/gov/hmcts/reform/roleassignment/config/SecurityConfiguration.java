fbf8655205edee16bbd0e0942a4ed8c0426d0d7f
dharmendra8186 <dharmendra.singh@hmcts.net>
2020-09-16 11:30:34 +0100

    AM-1021- changes for AKS deployment issues.

--- src/main/java/uk/gov/hmcts/reform/roleassignment/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/config/SecurityConfiguration.java
@@ -63,7 +63,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         "/welcome",
         "/actuator/health",
         "/actuator/health/liveness",
-        "/"
+        "/actuator"
     };
 
 
