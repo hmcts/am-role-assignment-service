d04452e91c3c54d50c9624adb9abcf628d4c256d
Nitish Singla <nitish.singla@hmcts.net>
2020-09-21 12:11:30 +0100

    AM-1020: Checking the env variable availabiliy in logs.

--- src/main/java/uk/gov/hmcts/reform/roleassignment/oidc/OIdcAdminConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/oidc/OIdcAdminConfiguration.java
@@ -1,14 +1,17 @@
 package uk.gov.hmcts.reform.roleassignment.oidc;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Component
+@Slf4j
 public class OIdcAdminConfiguration {
 
+    public static final String ROLE_ASSIGNMENT_IDAM_ADMIN_PASSWORD = "ROLE_ASSIGNMENT_IDAM_ADMIN_PASSWORD";
     private String userId;
     private String password;
     private String scope;
@@ -19,7 +22,11 @@ public class OIdcAdminConfiguration {
         @Value("${idam.client.admin.scope:}") String scope
     ) {
         this.userId = userId;
-        this.password = System.getenv("ROLE_ASSIGNMENT_IDAM_ADMIN_PASSWORD");
+        System.out.println("should_receive_response_for_add_role_assignment ROLE_ASSIGNMENT_IDAM_ADMIN_PASSWORD is"
+                               + System.getenv(ROLE_ASSIGNMENT_IDAM_ADMIN_PASSWORD));
+        log.error("should_receive_response_for_add_role_assignment ROLE_ASSIGNMENT_IDAM_ADMIN_PASSWORD is"
+                      + System.getenv(ROLE_ASSIGNMENT_IDAM_ADMIN_PASSWORD));
+        this.password = System.getenv(ROLE_ASSIGNMENT_IDAM_ADMIN_PASSWORD);
         this.scope = scope;
     }
 }
