59287c2238fc0b9b4c51977eb255ba8dc48c270f
Nitish Singla <nitish.singla@hmcts.net>
2020-09-22 12:08:45 +0100

    AM-1017: Code refactoring and clean up.

--- src/functionalTest/java/uk/gov/hmcts/reform/roleassignment/befta/RoleAssignmentTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/roleassignment/befta/RoleAssignmentTestAutomationAdapter.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.reform.roleassignment.befta;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
 import uk.gov.hmcts.befta.dse.ccd.TestDataLoaderToDefinitionStore;
 import uk.gov.hmcts.befta.player.BackEndFunctionalTestScenarioContext;
