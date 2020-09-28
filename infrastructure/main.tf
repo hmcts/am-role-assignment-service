a6fd9b2c44e8084863272f62def1df8286e52704
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-09-28 12:06:04 +0100

    Removed interpolation

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,51 +1,50 @@
 locals {
-  app_full_name = "${var.product}-${var.component}"
+  app_full_name = join("-", [var.product, var.component])
 
   local_env = (var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env
 
   // Vault name
-  previewVaultName = "${var.raw_product}-aat"
-  nonPreviewVaultName = "${var.raw_product}-${var.env}"
+  previewVaultName = join("-", [var.raw_product, "aat"])
+  nonPreviewVaultName = join("-", [var.raw_product, var.env])
   vaultName = (var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName
 
   // Shared Resource Group
-  previewResourceGroup = "${var.raw_product}-shared-infrastructure-aat"
-  nonPreviewResourceGroup = "${var.raw_product}-shared-infrastructure-${var.env}"
+  previewResourceGroup = join("-", [var.raw_product, "shared-infrastructure-aat"])
+  nonPreviewResourceGroup = join("-", [var.raw_product, "shared-infrastructure", var.env])
   sharedResourceGroup = (var.env == "preview" || var.env == "spreview") ? local.previewResourceGroup : local.nonPreviewResourceGroup
-
-  }
+}
 
 data "azurerm_key_vault" "am_key_vault" {
-  name = local.vaultName
+  name                = local.vaultName
   resource_group_name = local.sharedResourceGroup
 }
 
 data "azurerm_key_vault" "s2s_vault" {
-  name = "s2s-${local.local_env}"
-  resource_group_name = "rpe-service-auth-provider-${local.local_env}"
+  name                = join("-", ["s2s", local.local_env])
+  resource_group_name = join("-", ["rpe-service-auth-provider", local.local_env])
 }
 
 data "azurerm_key_vault_secret" "s2s_secret" {
-  name = "microservicekey-am-role-assignment-service"
-  key_vault_id = data.azurerm_key_vault.s2s_vault.id
+  name          = "microservicekey-am-role-assignment-service"
+  key_vault_id  = data.azurerm_key_vault.s2s_vault.id
 }
 
 resource "azurerm_key_vault_secret" "am_role_assignment_service_s2s_secret" {
-  name = "am-role-assignment-service-s2s-secret"
-  value = data.azurerm_key_vault_secret.s2s_secret.value
-  key_vault_id = data.azurerm_key_vault.am_key_vault.id
+  name          = "am-role-assignment-service-s2s-secret"
+  value         = data.azurerm_key_vault_secret.s2s_secret.value
+  key_vault_id  = data.azurerm_key_vault.am_key_vault.id
 }
 
 module "role-assignment-database" {
-  source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product = "${local.app_full_name}-postgres-db"
-  location = "${var.location}"
-  env = "${var.env}"
-  subscription = "${var.subscription}"
-  postgresql_user = "${var.postgresql_user}"
-  database_name = "${var.database_name}"
-  storage_mb = "${var.database_storage_mb}"
-  common_tags  = "${var.common_tags}"
+  source          = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  product         = join("-", [local.app_full_name, "postgres-db"])
+  location        = var.location}
+  env             = var.env
+  subscription    = var.subscription
+  postgresql_user = var.postgresql_user
+  database_name   = var.database_name
+  storage_mb      = var.database_storage_mb
+  common_tags     = var.common_tags
 }
 
 ////////////////////////////////
@@ -53,31 +52,31 @@ module "role-assignment-database" {
 ////////////////////////////////
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-  name = "${var.component}-POSTGRES-USER"
-  value = "${module.role-assignment-database.user_name}"
-  key_vault_id = "${data.azurerm_key_vault.am_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-USER"])
+  value         = module.role-assignment-database.user_name
+  key_vault_id  = data.azurerm_key_vault.am_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-  name = "${var.component}-POSTGRES-PASS"
-  value = "${module.role-assignment-database.postgresql_password}"
-  key_vault_id = "${data.azurerm_key_vault.am_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-PASS"])
+  value         = module.role-assignment-database.postgresql_password
+  key_vault_id  = data.azurerm_key_vault.am_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-  name = "${var.component}-POSTGRES-HOST"
-  value = "${module.role-assignment-database.host_name}"
-  key_vault_id = "${data.azurerm_key_vault.am_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-HOST"])
+  value         = module.role-assignment-database.host_name
+  key_vault_id  = data.azurerm_key_vault.am_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-  name = "${var.component}-POSTGRES-PORT"
-  value = "${module.role-assignment-database.postgresql_listen_port}"
-  key_vault_id = "${data.azurerm_key_vault.am_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-PORT"])
+  value         = module.role-assignment-database.postgresql_listen_port
+  key_vault_id  = data.azurerm_key_vault.am_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  name = "${var.component}-POSTGRES-DATABASE"
-  value = "${module.role-assignment-database.postgresql_database}"
-  key_vault_id = "${data.azurerm_key_vault.am_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-DATABASE"])
+  value         = module.role-assignment-database.postgresql_database
+  key_vault_id  = data.azurerm_key_vault.am_key_vault.id
 }
