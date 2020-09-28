238e38dc2316c3e587ad3baea6bebb341aeb9c06
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-09-28 16:33:06 +0100

    Removed provider to test

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -7,10 +7,10 @@ terraform {
   
   backend "azurerm" {}
 
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 2.25"
-    }
-  }
+  # required_providers {
+  #   azurerm = {
+  #     source = "hashicorp/azurerm"
+  #     version = "~> 2.25"
+  #   }
+  # }
 }
