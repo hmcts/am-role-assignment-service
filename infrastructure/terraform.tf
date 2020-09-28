e1c2eb89e8a501eb69b9e8a6a7f4f13f26def5dc
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-09-28 17:08:06 +0100

    Adding provider config

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -7,10 +7,22 @@ terraform {
   
   backend "azurerm" {}
 
-  # required_providers {
-  #   azurerm = {
-  #     source = "hashicorp/azurerm"
-  #     version = "~> 2.25"
-  #   }
-  # }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 2.25"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    external = {
+      source = "hashicorp/external"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
 }
