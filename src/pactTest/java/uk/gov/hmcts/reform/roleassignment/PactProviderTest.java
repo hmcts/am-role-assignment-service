4af7039ead438dbc92d8f1cf69fdbe3bfe297e27
ConorH994 <conor.halpin@hmcts.net>
2020-10-01 16:44:17 +0100

    adding pact provider test which will grab the pacts from specified location and run them

--- src/pactTest/java/uk/gov/hmcts/reform/roleassignment/PactProviderTest.java
+++ src/pactTest/java/uk/gov/hmcts/reform/roleassignment/PactProviderTest.java
@@ -1,4 +1,25 @@
 package uk.gov.hmcts.reform.roleassignment;
 
+import au.com.dius.pact.provider.junit.PactRunner;
+import au.com.dius.pact.provider.junit.target.HttpTarget;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.target.Target;
+import au.com.dius.pact.provider.junitsupport.target.TestTarget;
+
+import org.junit.runner.RunWith;
+
+@Provider("Idam_api")
+@PactBroker(host = "localhost", port = "9292")
+@RunWith(PactRunner.class)
 public class PactProviderTest {
+    //this fetches the stated provider pacts from the broker and runs them
+    @TestTarget
+    public final Target target = new HttpTarget();
+
+    @State("a list of roles are available in role assignment service")
+    public void listOfRoles() {
+        //empty
+    }
 }
