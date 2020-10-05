311034fa301caaf6a3fa15c84954bd61f96ba7fb
dharmendra8186 <dharmendra.singh@hmcts.net>
2020-10-05 17:26:36 +0100

    AM-1036-   adding new column in RAS.

--- src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/RoleAssignment.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/RoleAssignment.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.roleassignment.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
