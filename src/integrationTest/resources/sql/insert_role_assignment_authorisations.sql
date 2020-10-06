2ce32e5dc9840a3286a32a6f2d508bc406f35bd5
dharmendra8186 <dharmendra.singh@hmcts.net>
2020-10-06 15:47:49 +0100

    AM-1036-   Wrote IT for authorisation column based on dynamic query.

--- src/integrationTest/resources/sql/insert_role_assignment_authorisations.sql
+++ src/integrationTest/resources/sql/insert_role_assignment_authorisations.sql
@@ -2,8 +2,8 @@ DELETE FROM role_assignment_authorisations;
 
 INSERT INTO public.role_assignment_authorisations
 (authorisations, id)
-VALUES('dev', 'f7edb29d-e421-450c-be66-a10169b04f0a');
+VALUES('dev', '44276b66-11eb-42f5-a4dc-510fec18b0fb');
 
 INSERT INTO public.role_assignment_authorisations
 (authorisations, id)
-VALUES('tester', 'f7edb29d-e421-450c-be66-a10169b04f0a');
+VALUES('tester', '2ef8ebf3-266e-45d3-a3b8-4ce1e5d93b9f');
