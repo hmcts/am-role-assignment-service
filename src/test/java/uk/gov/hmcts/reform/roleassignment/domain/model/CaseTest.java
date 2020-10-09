29a6a44edd4dbd387d61f4c189e3351915ac1ba1
ConorH994 <conor.halpin@hmcts.net>
2020-10-09 13:22:36 +0100

    checkstyle

--- src/test/java/uk/gov/hmcts/reform/roleassignment/domain/model/CaseTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/domain/model/CaseTest.java
@@ -11,7 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
