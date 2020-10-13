6c0fcfa281755f0f386c8bdaab0aedc5d5a56ff3
dharmendra8186 <dharmendra.singh@hmcts.net>
2020-10-13 12:50:37 +0100

    AM-1061-  Fixed Sonar issue.

--- src/test/java/uk/gov/hmcts/reform/roleassignment/domain/service/common/ValidationModelServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/domain/service/common/ValidationModelServiceTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.roleassignment.domain.service.common;
 
-import com.microsoft.applicationinsights.boot.dependencies.google.common.collect.Sets;
 import lombok.Getter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,7 +8,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import uk.gov.hmcts.reform.roleassignment.domain.model.AssignmentRequest;
-import uk.gov.hmcts.reform.roleassignment.domain.model.QueryRequest;
 import uk.gov.hmcts.reform.roleassignment.domain.model.Role;
 import uk.gov.hmcts.reform.roleassignment.domain.model.RoleAssignment;
 import uk.gov.hmcts.reform.roleassignment.domain.model.enums.Status;
@@ -18,15 +16,16 @@ import uk.gov.hmcts.reform.roleassignment.helper.TestDataBuilder;
 import uk.gov.hmcts.reform.roleassignment.util.SecurityUtils;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
@@ -83,21 +82,12 @@ class ValidationModelServiceTest {
         actorIds.add("123e4567-e89b-42d3-a456-556642445678");
         requestActorIds.add("4dc7dd3c-3fb5-4611-bbde-5101a97681e1");
 
-        HashMap<String, List<String>> attributes = new HashMap<>();
-        attributes.put("jurisdiction", Arrays.asList("IA"));
-
-        QueryRequest queryRequest = QueryRequest.builder()
-            .actorId(List.copyOf(Sets.union(actorIds, requestActorIds)))
-            .roleName(Arrays.asList("senior-tribunal-caseworker", "tribunal-caseworker"))
-            .roleType(Arrays.asList("ORGANISATION"))
-            .attributes(attributes)
-            .build();
-
-        when(persistenceService.retrieveRoleAssignmentsByQueryRequest(queryRequest, 0, 0, null, null))
+        when(persistenceService.retrieveRoleAssignmentsByQueryRequest(any(), anyInt(), anyInt(), any(), any()))
             .thenReturn((List<RoleAssignment>) TestDataBuilder.buildRequestedRoleCollection(Status.LIVE));
         Set<Object> facts = new HashSet<>();
         sut.executeQueryParamForCaseRole(facts, actorIds, requestActorIds);
         assertNotNull(facts);
+        assertEquals(facts.size(),2);
 
 
     }
