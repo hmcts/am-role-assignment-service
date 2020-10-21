d0d577a5503203b20f166e3d9578db954fa97b03
ConorH994 <conor.halpin@hmcts.net>
2020-10-21 12:31:58 +0100

    successfully filtering and handling correlation Id for each request

---
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/MutableHttpServletRequest.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.roleassignment.domain.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+//this is used in the filter so that correlation Id can be added to the request
+public final class MutableHttpServletRequest extends HttpServletRequestWrapper {
+    // holds custom header and value mapping
+    private final Map<String, String> customHeaders;
+
+    public MutableHttpServletRequest(HttpServletRequest request){
+        super(request);
+        this.customHeaders = new HashMap<>();
+    }
+
+    public void putHeader(String name, String value){
+        this.customHeaders.put(name, value);
+    }
+
+    @Override
+    public String getHeader(String name) {
+        // check the custom headers first
+        String headerValue = customHeaders.get(name);
+
+        if (headerValue != null){
+            return headerValue;
+        }
+        // else return from into the original wrapped object
+        return ((HttpServletRequest) getRequest()).getHeader(name);
+    }
+}
