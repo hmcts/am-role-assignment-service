03727cc677eb7866a518d351ea8c64ff1d100b3f
ConorH994 <conor.halpin@hmcts.net>
2020-10-22 13:38:06 +0100

    removing suppression

--- src/main/java/uk/gov/hmcts/reform/roleassignment/util/FilterRequestUtil.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/util/FilterRequestUtil.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@SuppressWarnings("squid:S5167")
 @Component
 public class FilterRequestUtil extends OncePerRequestFilter {
 
