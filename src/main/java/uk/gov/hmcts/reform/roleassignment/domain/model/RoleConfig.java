ddc69bf63dd1fcd05e381c8b2778caaa4166f15e
Jon Moore <jonathan.moore@hmcts.net>
2020-10-26 15:32:39 +0000

    Restructure rules and rule process.
    Removed Java dependencies on IAC-specific role details.
    
    Reinstated the rule-driven mechanism for retrieving case data.
    
    Restructured rules for efficiency and separability of the rules for
    different services.

---
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/RoleConfig.java
@@ -0,0 +1,69 @@
+package uk.gov.hmcts.reform.roleassignment.domain.model;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import uk.gov.hmcts.reform.roleassignment.domain.model.enums.RoleCategory;
+import uk.gov.hmcts.reform.roleassignment.util.JacksonUtils;
+
+@Slf4j
+public class RoleConfig {
+
+	@Getter
+    private static RoleConfig roleConfig = buildRoleConfig();
+
+	private final Map<String,RoleConfigRole> roleConfigByRoleName = new HashMap<>();
+
+	private RoleConfig(Collection<RoleConfigRole> roles) {
+		roles.forEach(r -> roleConfigByRoleName.put(r.getName(), r));
+	}
+
+	public RoleConfigRole get(String roleName) {
+		return roleConfigByRoleName.get(roleName);
+	}
+
+	/**
+	 * Copy the role name and category into each of the patterns
+	 * for the given role.
+	 */
+	private static void setRoleNameAndCategory(RoleConfigRole role) {
+		String roleName = role.getName();
+		RoleCategory roleCategory = role.getCategory();
+		role.getPatterns().forEach(
+				p -> {
+					p.getData().setRoleName(roleName);
+					p.getData().setRoleCategory(roleCategory);
+				});
+	}
+
+    private static RoleConfig buildRoleConfig() {
+        InputStream input = JacksonUtils.class.getClassLoader().getResourceAsStream("role.json");
+        CollectionType listType = JacksonUtils.MAPPER.getTypeFactory().constructCollectionType(
+            ArrayList.class,
+            RoleConfigRole.class
+        );
+        List<RoleConfigRole> allRoles = new ArrayList<>();
+        try {
+            allRoles = JacksonUtils.MAPPER.readValue(input, listType);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        allRoles.forEach(RoleConfig::setRoleNameAndCategory);
+        return new RoleConfig(allRoles);
+    }
+
+	// ----------------------------------------------------- TEST CODE -------------------------------------------------------
+
+    public static void main(String[] args) {
+    	System.out.println(getRoleConfig().get("judge"));
+    }
+}
