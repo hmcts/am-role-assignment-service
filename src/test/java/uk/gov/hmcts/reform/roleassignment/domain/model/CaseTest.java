be413ea4ad89e9f57cf6828ddde8694dc29a3b3b
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-11-09 19:59:00 +0000

    updated the case object structure

--- src/test/java/uk/gov/hmcts/reform/roleassignment/domain/model/CaseTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/domain/model/CaseTest.java
@@ -6,21 +6,14 @@ import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.time.LocalDateTime;
 
 @RunWith(MockitoJUnitRunner.class)
 class CaseTest {
 
 
-    private Case caseData = new Case("1234", (long)1, 1, "", "",
-                                     LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now(),"",
-                                     null,null,null,
-                                     null,null, null);
+    private Case caseData = new Case("1234", (long)1, 1, "", "");
 
     @BeforeEach
     public void setUp() {
@@ -31,17 +24,5 @@ class CaseTest {
     void testToString() {
         assertNotNull(caseData.toString());
         assertTrue(caseData.toString().contains("1234"));
-    }
-
-    @Test
-    void hasCaseReference() {
-        assertTrue(caseData.hasCaseReference());
-        assertEquals(1L, caseData.getReference());
-    }
-
-    @Test
-    void hasCaseReferenceFalse() {
-        caseData.setReference(null);
-        assertFalse(caseData.hasCaseReference());
     }
 }
