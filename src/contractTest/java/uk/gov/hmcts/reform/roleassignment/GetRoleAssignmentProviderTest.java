9720f86182c9f76d5cf9de7852a688d6b3b827f0
Nitin <nitin.sahu@hmcts.net>
2020-11-27 13:30:04 +0000

    AM-981 : Updated Provider pact for CREATE

--- src/contractTest/java/uk/gov/hmcts/reform/roleassignment/GetRoleAssignmentProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/roleassignment/GetRoleAssignmentProviderTest.java
@@ -11,10 +11,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.roleassignment.controller.endpoints.CreateAssignmentController;
 import uk.gov.hmcts.reform.roleassignment.controller.endpoints.GetAssignmentController;
+import uk.gov.hmcts.reform.roleassignment.domain.service.common.ParseRequestService;
+import uk.gov.hmcts.reform.roleassignment.domain.service.common.ValidationModelService;
+import uk.gov.hmcts.reform.roleassignment.domain.service.createroles.CreateRoleAssignmentOrchestrator;
+import uk.gov.hmcts.reform.roleassignment.domain.service.createroles.CreateRoleAssignmentService;
 import uk.gov.hmcts.reform.roleassignment.domain.service.getroles.RetrieveRoleAssignmentOrchestrator;
 import org.springframework.context.annotation.Import;
+import uk.gov.hmcts.reform.roleassignment.util.PersistenceUtil;
 
 import java.io.IOException;
 
@@ -23,10 +30,15 @@ import java.io.IOException;
 @PactBroker(scheme = "${PACT_BROKER_SCHEME:http}", host = "${PACT_BROKER_URL:localhost}",
     port = "${PACT_BROKER_PORT:9292}")
 @Import(RoleAssignmentProviderTestConfiguration.class)
-public class RoleAssignmentProviderTest {
+public class GetRoleAssignmentProviderTest {
 
     @Autowired
     private RetrieveRoleAssignmentOrchestrator retrieveRoleAssignmentServiceMock;
+
+
+    @Autowired
+    private CreateRoleAssignmentOrchestrator createRoleAssignmentOrchestrator;
+
 
     @TestTemplate
     @ExtendWith(PactVerificationInvocationContextProvider.class)
@@ -34,7 +46,6 @@ public class RoleAssignmentProviderTest {
         context.verifyInteraction();
     }
 
-    @BeforeEach
     void before(PactVerificationContext context) {
         MockMvcTestTarget testTarget = new MockMvcTestTarget();
         System.getProperties().setProperty("pact.verifier.publishResults", "true");
@@ -45,8 +56,21 @@ public class RoleAssignmentProviderTest {
 
     }
 
+    void beforeCreate(PactVerificationContext context) {
+        MockMvcTestTarget testTarget = new MockMvcTestTarget();
+        System.getProperties().setProperty("pact.verifier.publishResults", "true");
+        testTarget.setControllers(new CreateAssignmentController(
+            createRoleAssignmentOrchestrator
+        ));
+        context.setTarget(testTarget);
+
+    }
     @State({"A list of roles are available in role assignment service"})
     public void toGetlistOfRolesWithSuccess() throws IOException, JSONException {
+    }
 
+    @State({"The assignment request is valid with one requested role and replaceExisting flag as false"})
+    public void toGetlistOfRolesWithSuccess1() throws IOException, JSONException {
+        System.out.println("toGetlistOfRolesWithSuccess.......");
     }
 }
--- src/contractTest/java/uk/gov/hmcts/reform/roleassignment/RoleAssignmentProviderTestConfiguration.java
+++ src/contractTest/java/uk/gov/hmcts/reform/roleassignment/RoleAssignmentProviderTestConfiguration.java
@@ -1,12 +1,29 @@
 package uk.gov.hmcts.reform.roleassignment;
 
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.StatelessKieSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import uk.gov.hmcts.reform.roleassignment.data.RequestEntity;
+import uk.gov.hmcts.reform.roleassignment.domain.service.common.ParseRequestService;
 import uk.gov.hmcts.reform.roleassignment.domain.service.common.PersistenceService;
 import uk.gov.hmcts.reform.roleassignment.domain.service.common.PrepareResponseService;
+import uk.gov.hmcts.reform.roleassignment.domain.service.common.RetrieveDataService;
+import uk.gov.hmcts.reform.roleassignment.domain.service.common.ValidationModelService;
+import uk.gov.hmcts.reform.roleassignment.domain.service.createroles.CreateRoleAssignmentOrchestrator;
+import uk.gov.hmcts.reform.roleassignment.domain.service.createroles.CreateRoleAssignmentService;
 import uk.gov.hmcts.reform.roleassignment.domain.service.getroles.RetrieveRoleAssignmentOrchestrator;
+import uk.gov.hmcts.reform.roleassignment.feignclients.DataStoreFeignClient;
+import uk.gov.hmcts.reform.roleassignment.util.CorrelationInterceptorUtil;
+import uk.gov.hmcts.reform.roleassignment.util.PersistenceUtil;
+import uk.gov.hmcts.reform.roleassignment.util.SecurityUtils;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 
 @TestConfiguration
@@ -15,12 +32,68 @@ public class RoleAssignmentProviderTestConfiguration {
     @MockBean
     private PersistenceService persistenceService;
 
+    @Bean
+    @Primary
+    public PrepareResponseService getPrepareResponseService() {
+        return new PrepareResponseService();
+    }
+
     @MockBean
-    private PrepareResponseService prepareResponseService;
+    private CorrelationInterceptorUtil correlationInterceptorUtil;
+
+    @Bean
+    @Primary
+    public ParseRequestService getParseRequestService() {
+        return new ParseRequestService();
+    }
+
+    private KieServices kieServices = KieServices.Factory.get();
+
+    @Bean
+    public KieContainer kieContainer() {
+        return kieServices.getKieClasspathContainer();
+    }
+
+    @MockBean
+    SecurityUtils securityUtils;
+
+
+    @Bean
+    public StatelessKieSession getStatelessKieSession() {
+        return kieContainer().newStatelessKieSession("role-assignment-validation-session");
+    }
+
+    @MockBean
+    private  DataStoreFeignClient dataStoreFeignClient;
+
+    @Bean
+    public RetrieveDataService getRetrieveDataService() {
+        return new RetrieveDataService(dataStoreFeignClient);
+    }
+
+    @Bean
+    @Primary
+    public ValidationModelService getValidationModelService() {
+        return new ValidationModelService(getStatelessKieSession(), getRetrieveDataService(), persistenceService);
+    }
+
+    @Bean
+    @Primary
+    public PersistenceUtil getPersistenceUtil() {
+        return new PersistenceUtil();
+    }
 
     @Bean
     @Primary
     public RetrieveRoleAssignmentOrchestrator getListOfRoles() {
-        return new RetrieveRoleAssignmentOrchestrator(persistenceService, prepareResponseService);
+        return new RetrieveRoleAssignmentOrchestrator(persistenceService, getPrepareResponseService());
     }
+
+    @Bean
+    @Primary
+    public CreateRoleAssignmentOrchestrator createRoleAssignment() {
+        return new CreateRoleAssignmentOrchestrator(getParseRequestService(), getPrepareResponseService(), persistenceService,
+                                                    getValidationModelService(), getPersistenceUtil());
+    }
+
 }
