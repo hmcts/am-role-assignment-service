42ee141ad9c461757a3954d36dfee2f2b2df17e9
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-12-02 15:51:35 +0000

    adding the definition file import for ccd-case retrieval

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/roleassignment/befta/TestDataLoaderToDefinitionStoreRAS.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.reform.roleassignment.befta;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.hmcts.befta.TestAutomationAdapter;
+import uk.gov.hmcts.befta.dse.ccd.CcdRoleConfig;
+import uk.gov.hmcts.befta.dse.ccd.TestDataLoaderToDefinitionStore;
+
+import java.io.IOException;
+
+@Slf4j
+public class TestDataLoaderToDefinitionStoreRAS extends TestDataLoaderToDefinitionStore {
+    private static final Logger logger = LoggerFactory.getLogger(TestDataLoaderToDefinitionStoreRAS.class);
+
+    public TestDataLoaderToDefinitionStoreRAS(TestAutomationAdapter adapter) {
+        super(adapter);
+    }
+
+    protected void addNewCcdRole(CcdRoleConfig roleConfig) {
+        logger.info("\nAdding the role {}", roleConfig);
+        addCcdRole(roleConfig);
+        logger.info("\nAdded the role {}", roleConfig);
+    }
+
+    protected void importNewDefinition(String fileResourcePath) throws IOException {
+        logger.info("\nImporting the file {}", fileResourcePath);
+        importDefinition(fileResourcePath);
+        logger.info("\nFile imported successfully");
+    }
+}
