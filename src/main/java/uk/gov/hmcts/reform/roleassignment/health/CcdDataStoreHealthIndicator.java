63e0f5bcf6708f27078ae97aff49c04c327676c1
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-12-10 16:08:20 +0000

    Implemented health checks for downstream systems Idam and ccd

---
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/health/CcdDataStoreHealthIndicator.java
@@ -0,0 +1,38 @@
+package uk.gov.hmcts.reform.roleassignment.health;
+
+
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthContributor;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@Slf4j
+public class CcdDataStoreHealthIndicator implements HealthIndicator, HealthContributor {
+
+    private RestTemplate restTemplate;
+    @Value("${feign.client.config.datastoreclient.url:}") String URL;
+
+    @Autowired
+    public CcdDataStoreHealthIndicator(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public Health health() {
+        try {
+            JsonNode resp = restTemplate.getForObject(URL + "/health", JsonNode.class);
+            if (resp.get("status").asText().equalsIgnoreCase("UP")) {
+                return Health.up().build();
+            }
+        } catch (Exception ex) {
+            return Health.down(ex).build();
+        }
+        return Health.down().build();
+    }
+}
