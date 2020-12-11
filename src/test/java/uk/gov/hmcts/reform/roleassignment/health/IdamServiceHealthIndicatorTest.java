f88c21e0d87b9084bef2e1a0c54185beb6e37db6
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-12-11 16:34:18 +0000

    unit testing

---
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/health/IdamServiceHealthIndicatorTest.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.reform.roleassignment.health;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class IdamServiceHealthIndicatorTest {
+
+    private RestTemplate restTemplate = mock(RestTemplate.class);
+
+    private IdamServiceHealthIndicator sut = new IdamServiceHealthIndicator(restTemplate);
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void health() throws JsonProcessingException {
+        String jsonString = "{\"status\": \"UP\"}";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode actualObj = mapper.readTree(jsonString);
+        when(sut.health()).thenReturn(Health.up().build());
+        when(restTemplate.getForObject("any_url",JsonNode.class)).thenReturn(actualObj);
+        Health health = sut.health();
+        assertNotNull(sut.checkServiceHealth(restTemplate,"url"));
+    }
+}
