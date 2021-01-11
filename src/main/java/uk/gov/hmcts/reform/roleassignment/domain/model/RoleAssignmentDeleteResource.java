a4c33a4b9292706d60b36a4c7d4afdd4f77300e3
dharmendra8186 <dharmendra.singh@hmcts.net>
2021-01-11 16:47:32 +0000

    AM-1161 remove link from response in ras.

--- src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/RoleAssignmentDeleteResource.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/RoleAssignmentDeleteResource.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.roleassignment.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,6 +15,7 @@ import org.springframework.validation.annotation.Validated;
 @NoArgsConstructor
 @Validated
 @Slf4j
+@JsonIgnoreProperties(value = { "links" })
 public class RoleAssignmentDeleteResource extends RepresentationModel<RoleAssignmentDeleteResource> {
 
 
