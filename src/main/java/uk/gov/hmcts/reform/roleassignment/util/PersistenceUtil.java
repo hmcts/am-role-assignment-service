1d47702fac2fbd0388976a308e022080ea87205a
ConorH994 <conor.halpin@hmcts.net>
2021-01-21 13:26:37 +0000

    history logs for create requested entry added

--- src/main/java/uk/gov/hmcts/reform/roleassignment/util/PersistenceUtil.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/util/PersistenceUtil.java
@@ -163,8 +163,6 @@ public class PersistenceUtil {
         UUID roleAssignmentId = roleAssignment.getId();
         UUID requestId = request.getId();
 
-        request.setLog("Create Requested with replace existing"); //TODO This does not set the log for the history
-
         RequestEntity requestEntity = convertRequestToEntity(request);
         if (requestId != null) {
             requestEntity.setId(requestId);
