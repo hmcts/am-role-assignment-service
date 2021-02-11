ecd6d92c33fb33f4eee6aad9789547042286a123
ConorH994 <conor.halpin@hmcts.net>
2021-02-11 15:44:12 +0000

    checkstyle changes

--- src/test/java/uk/gov/hmcts/reform/roleassignment/domain/model/RoleConfigConstraintTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/domain/model/RoleConfigConstraintTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.Collections;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Collections;
 
 @RunWith(MockitoJUnitRunner.class)
 class RoleConfigConstraintTest {
