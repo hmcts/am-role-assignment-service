2bcab267b04a214d481f507c1f557522af0958e9
dharmendra8186 <dharmendra.singh@hmcts.net>
2021-02-23 12:58:30 +0000

    AM-1216: Fixed Detached entity execption

--- src/main/java/uk/gov/hmcts/reform/roleassignment/domain/service/deleteroles/DeleteRoleAssignmentOrchestrator.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/domain/service/deleteroles/DeleteRoleAssignmentOrchestrator.java
@@ -62,7 +62,7 @@ public class DeleteRoleAssignmentOrchestrator {
         this.persistenceUtil = persistenceUtil;
     }
 
-
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public ResponseEntity<Void> deleteRoleAssignmentByProcessAndReference(String process,
                                                                             String reference) {
         long startTime = System.currentTimeMillis();
@@ -109,6 +109,7 @@ public class DeleteRoleAssignmentOrchestrator {
         return responseEntity;
     }
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public ResponseEntity<Void> deleteRoleAssignmentByAssignmentId(String assignmentId) {
         List<RoleAssignment> requestedRoles;
 
