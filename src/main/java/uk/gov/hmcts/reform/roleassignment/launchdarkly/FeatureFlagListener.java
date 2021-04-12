4a2d275dfe30a93c94ad34c4df60fbc99d9d5ae9
dharmendra8186 <dharmendra.singh@hmcts.net>
2021-04-12 17:11:15 +0100

    AM_1282: Fix the sonar issues.

--- src/main/java/uk/gov/hmcts/reform/roleassignment/launchdarkly/FeatureFlagListener.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/launchdarkly/FeatureFlagListener.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.roleassignment.controller.endpoints.CreateAssignmentController;
 import uk.gov.hmcts.reform.roleassignment.util.LDEventListener;
 
 import java.util.Map;
