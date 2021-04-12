a602dbdfa0a55362c8f6b612b9e5f68ee04a822b
dharmendra8186 <dharmendra.singh@hmcts.net>
2021-04-12 16:52:59 +0100

    AM_1282: Fix the sonar issues.

--- src/test/java/uk/gov/hmcts/reform/roleassignment/util/LDEventListenerTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/util/LDEventListenerTest.java
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class LDEventListenerTest {
+class LDEventListenerTest {
 
 
     @Mock
