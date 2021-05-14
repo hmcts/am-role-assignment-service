180efcaea206471ede4e1240a613c18b59d5483c
Nitish Singla <nitish.singla@hmcts.net>
2021-05-14 12:22:27 +0100

    Added more application logs, around token validation.

--- src/main/java/uk/gov/hmcts/reform/roleassignment/config/SecurityEndpointFilter.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/config/SecurityEndpointFilter.java
@@ -1,8 +1,12 @@
 package uk.gov.hmcts.reform.roleassignment.config;
 
 import feign.FeignException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
+import uk.gov.hmcts.reform.idam.client.IdamApi;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
+import uk.gov.hmcts.reform.roleassignment.util.Constants;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -13,11 +17,27 @@ import java.io.IOException;
 @Configuration
 public class SecurityEndpointFilter extends OncePerRequestFilter {
 
+    public static final String AUTHORIZATION = "Authorization";
+
+    @Autowired
+    IdamApi idamApi;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
         throws ServletException, IOException {
         try {
+            logger.debug("The Service Auth token length is: "
+                             + request.getHeader(Constants.SERVICE_AUTHORIZATION2).length());
+            logger.debug("The User Auth token length : "
+                             + request.getHeader(AUTHORIZATION).length());
+            logger.debug("The User Auth token contains 'Bearer '? : "
+                             + request.getHeader(AUTHORIZATION).contains("Bearer "));
+            if (logger.isDebugEnabled()) {
+                UserInfo userInfo = idamApi.retrieveUserInfo(request.getHeader(AUTHORIZATION));
+                logger.debug(userInfo);
+            }
             filterChain.doFilter(request, response);
+
         } catch (Exception e) {
             Throwable throwable = e.getCause();
             if (throwable instanceof FeignException.FeignClientException) {
