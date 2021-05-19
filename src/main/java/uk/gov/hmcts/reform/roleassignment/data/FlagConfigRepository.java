eaeff11f4be2267f0274b65b539ef7b3c6e13c33
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2021-05-19 15:40:03 +0100

    Feature to flip the db feature flag through environment variables

--- src/main/java/uk/gov/hmcts/reform/roleassignment/data/FlagConfigRepository.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/data/FlagConfigRepository.java
@@ -1,15 +1,10 @@
 package uk.gov.hmcts.reform.roleassignment.data;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FlagConfigRepository extends CrudRepository<FlagConfig, Long> {
 
-    @Query("select fc from flag_config as fc"
-        + " where fc.flagName=?1 "
-        + " and upper(fc.env) = upper(?2)")
-    FlagConfig getStatusByParams(String flagName, String envName);
-
+    FlagConfig findByFlagNameAndEnv(String flagName, String envName);
 }
