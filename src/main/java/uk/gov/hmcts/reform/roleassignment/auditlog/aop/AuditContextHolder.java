bb5fb09feb457d377f516bec2452eb432589de80
Sankar Padakula <sankar.padakula@hmcts.net>
2021-06-10 10:47:39 +0100

    Mutation test fixes

--- src/main/java/uk/gov/hmcts/reform/roleassignment/auditlog/aop/AuditContextHolder.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/auditlog/aop/AuditContextHolder.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.roleassignment.auditlog.aop;
 public class AuditContextHolder {
 
     private AuditContextHolder(){
+
     }
 
     private static final InheritableThreadLocal<AuditContext> threadLocal = new InheritableThreadLocal<>();
