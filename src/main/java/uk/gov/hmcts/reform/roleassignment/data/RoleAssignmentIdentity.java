4bfeef75451ebb3b5156d585a4e3aa93803b220c
Nitish Singla <nitish.singla@hmcts.net>
2021-06-17 10:26:30 +0100

    (fix): Fixing the tests.

--- src/main/java/uk/gov/hmcts/reform/roleassignment/data/RoleAssignmentIdentity.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/data/RoleAssignmentIdentity.java
@@ -1,16 +1,16 @@
 
 package uk.gov.hmcts.reform.roleassignment.data;
 
-import javax.persistence.Embeddable;
-import java.io.Serializable;
-import java.util.UUID;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -22,9 +22,7 @@ import lombok.Setter;
 public class RoleAssignmentIdentity implements Serializable {
 
     private UUID id;
-
     private String status;
-
     private UUID requestEntity;
 }
 
