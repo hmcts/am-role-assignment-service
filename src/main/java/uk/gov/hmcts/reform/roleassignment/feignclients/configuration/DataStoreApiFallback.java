abfb8114797553f55af458ea2685d5a73258f3d3
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2021-06-21 13:38:41 +0100

    Updated role.json and drool rule configuration to support ccd case roles.

--- src/main/java/uk/gov/hmcts/reform/roleassignment/feignclients/configuration/DataStoreApiFallback.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/feignclients/configuration/DataStoreApiFallback.java
@@ -1,10 +1,10 @@
 package uk.gov.hmcts.reform.roleassignment.feignclients.configuration;
 
-import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.roleassignment.feignclients.DataStoreApi;
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.roleassignment.domain.model.Case;
+import uk.gov.hmcts.reform.roleassignment.feignclients.DataStoreApi;
 
-@Component
+@Service
 public class DataStoreApiFallback implements DataStoreApi {
 
     public static final String DATA_STORE_NOT_AVAILABLE = "The data store Service is not available";
@@ -16,9 +16,19 @@ public class DataStoreApiFallback implements DataStoreApi {
 
     @Override
     public Case getCaseDataV2(String caseId) {
-        return Case.builder().id(caseId)
-            .caseTypeId("Asylum")
-            .jurisdiction("IA")
-            .build();
+        Case dummyCase = null;
+
+        if (caseId.equals("1234567890123456")){
+            dummyCase = Case.builder().id(caseId)
+                .caseTypeId("CARE_SUPERVISION_EPO")
+                .jurisdiction("PUBLICLAW")
+                .build();
+        } else {
+            dummyCase = Case.builder().id(caseId)
+                .caseTypeId("Asylum")
+                .jurisdiction("IA")
+                .build();
+        }
+        return dummyCase;
     }
 }
