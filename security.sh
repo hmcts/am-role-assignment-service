4ef16da6f12b95397989a4722721942caf2d3a1e
ConorH994 <conor.halpin@hmcts.net>
2021-06-30 13:11:34 +0100

    fixing zap cli

--- security.sh
+++ security.sh
@@ -4,6 +4,8 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL
 cat zap.out
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 echo "ZAP has successfully started"
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
 mkdir -p functional-output
