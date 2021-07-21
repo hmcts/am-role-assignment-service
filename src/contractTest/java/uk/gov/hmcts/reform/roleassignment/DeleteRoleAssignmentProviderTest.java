3c3fb62fc2a1c2ecac587fd1c19f7c2a58522c50
ConorH994 <conor.halpin@hmcts.net>
2021-07-21 22:20:09 +0100

    clean up

--- src/contractTest/java/uk/gov/hmcts/reform/roleassignment/DeleteRoleAssignmentProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/roleassignment/DeleteRoleAssignmentProviderTest.java
@@ -73,6 +73,7 @@ public class DeleteRoleAssignmentProviderTest {
     private DeleteRoleAssignmentOrchestrator deleteRoleAssignmentOrchestrator;
 
     private static final String ASSIGNMENT_ID = "704c8b1c-e89b-436a-90f6-953b1dc40157";
+    private static final String AUTH_USER_ID = "3168da13-00b3-41e3-81fa-cbc71ac28a0f";
 
     @TestTemplate
     @ExtendWith(PactVerificationInvocationContextProvider.class)
@@ -85,6 +86,7 @@ public class DeleteRoleAssignmentProviderTest {
     @BeforeEach
     void beforeCreate(PactVerificationContext context) {
         MockMvcTestTarget testTarget = new MockMvcTestTarget();
+        System.getProperties().setProperty("pact.verifier.publishResults", "true");
         testTarget.setControllers(new DeleteAssignmentController(
             deleteRoleAssignmentOrchestrator
         ));
@@ -95,60 +97,38 @@ public class DeleteRoleAssignmentProviderTest {
 
     @State({"An actor with provided id is available in role assignment service"})
     public void deleteRoleAssignmentById() throws IOException {
-        setInitMockId();
+        initMocksId();
     }
 
     @State({"An actor with provided process & reference is available in role assignment service"})
     public void deleteRoleAssignmentByPr() throws IOException {
-        setInitMockPr();
+        initMocksPr();
     }
 
-    private void setInitMockId() throws IOException {
-        Request deleteRequest = TestDataBuilder.buildRequest(Status.LIVE, false);
-        List<RoleAssignment> roleAssignmentList = TestDataBuilder
-            .buildRoleAssignmentList_Custom(Status.LIVE, "1234", "attributesCase.json", RoleType.CASE,
-                                            "tribunal-caseworker");
-
-        when(securityUtils.getServiceName()).thenReturn("am_org_role_mapping_service");
-        when(persistenceService.persistRequest(any())).thenReturn(TestDataBuilder.buildRequestEntity(deleteRequest));
+    private void initMocksId() throws IOException {
+        initCommonMocks();
         when(persistenceService.getAssignmentById(UUID.fromString(ASSIGNMENT_ID)))
-            .thenReturn(roleAssignmentList);
-        when(persistenceService.getStatusByParam(FeatureFlagEnum.IAC_1_0.getValue(), "pr")).thenReturn(true);
-        when(securityUtils.getUserId()).thenReturn("3168da13-00b3-41e3-81fa-cbc71ac28a0f");
-
-        JsonNode attributes = buildAttributesFromFile("attributesCase.json");
-        Map<String, JsonNode> attributeMap = JacksonUtils.convertValue(attributes);
-        List<Assignment> assignmentList  = new ArrayList<>();
-        assignmentList.add(ExistingRoleAssignment.builder().actorId("3168da13-00b3-41e3-81fa-cbc71ac28a0f")
-                               .roleType(RoleType.ORGANISATION).roleName("tribunal-caseworker").attributes(attributeMap)
-                               .status(Status.APPROVED).build());
-        when(persistenceService.persistRequest(any())).thenReturn(createEntity());
-        doReturn(assignmentList).when(persistenceService)
-            .retrieveRoleAssignmentsByQueryRequest(any(), anyInt(), anyInt(), any(), any(), anyBoolean());
-        when(persistenceService.getTotalRecords()).thenReturn(1L);
-
-        when(dataStoreApi.getCaseDataV2(anyString())).thenReturn(Case.builder().id("1212121212121213").jurisdiction(
-            "IA").caseTypeId("Asylum").build());
+            .thenReturn(roleAssignmentList());
 
     }
 
-    private void setInitMockPr() throws IOException {
-        Request deleteRequest = TestDataBuilder.buildRequest(Status.LIVE, false);
-        List<RoleAssignment> roleAssignmentList = TestDataBuilder
-            .buildRoleAssignmentList_Custom(Status.LIVE,"1234","attributesCase.json", RoleType.CASE,
-                                            "tribunal-caseworker");
-
-        when(securityUtils.getServiceName()).thenReturn("am_org_role_mapping_service");
-        when(persistenceService.persistRequest(any())).thenReturn(TestDataBuilder.buildRequestEntity(deleteRequest));
+    private void initMocksPr() throws IOException {
+        initCommonMocks();
         when(persistenceService.getAssignmentsByProcess("p2", "r2", Status.LIVE.toString()))
-            .thenReturn(roleAssignmentList);
+            .thenReturn(roleAssignmentList());
+
+    }
+
+    private void initCommonMocks() {
+        when(persistenceService.persistRequest(any())).thenReturn(TestDataBuilder.buildRequestEntity(deleteRequest()));
+        when(securityUtils.getServiceName()).thenReturn("am_org_role_mapping_service");
         when(persistenceService.getStatusByParam(FeatureFlagEnum.IAC_1_0.getValue(), "pr")).thenReturn(true);
-        when(securityUtils.getUserId()).thenReturn("3168da13-00b3-41e3-81fa-cbc71ac28a0f");
+        when(securityUtils.getUserId()).thenReturn(AUTH_USER_ID);
 
         JsonNode attributes = buildAttributesFromFile("attributesCase.json");
         Map<String, JsonNode> attributeMap = JacksonUtils.convertValue(attributes);
         List<Assignment> assignmentList  = new ArrayList<>();
-        assignmentList.add(ExistingRoleAssignment.builder().actorId("3168da13-00b3-41e3-81fa-cbc71ac28a0f")
+        assignmentList.add(ExistingRoleAssignment.builder().actorId(AUTH_USER_ID)
                                .roleType(RoleType.ORGANISATION).roleName("tribunal-caseworker").attributes(attributeMap)
                                .status(Status.APPROVED).build());
         when(persistenceService.persistRequest(any())).thenReturn(createEntity());
@@ -158,13 +138,23 @@ public class DeleteRoleAssignmentProviderTest {
 
         when(dataStoreApi.getCaseDataV2(anyString())).thenReturn(Case.builder().id("1212121212121213").jurisdiction(
             "IA").caseTypeId("Asylum").build());
+    }
+
+    private List<RoleAssignment> roleAssignmentList() throws IOException {
+        return TestDataBuilder
+            .buildRoleAssignmentList_Custom(Status.LIVE,"1234",
+                                            "attributesCase.json", RoleType.CASE, "tribunal-caseworker");
+    }
+
+    private Request deleteRequest() {
+        return TestDataBuilder.buildRequest(Status.LIVE, false);
     }
 
     public RequestEntity createEntity() {
         return RequestEntity.builder()
             .correlationId("123")
             .id(UUID.fromString("c3552563-80e1-49a1-9dc9-b2625e7c44dc"))
-            .authenticatedUserId("3168da13-00b3-41e3-81fa-cbc71ac28a0f")
+            .authenticatedUserId(AUTH_USER_ID)
             .clientId("am_org_role_mapping_service")
             .created(LocalDateTime.now())
             .status(Status.APPROVED.toString())
