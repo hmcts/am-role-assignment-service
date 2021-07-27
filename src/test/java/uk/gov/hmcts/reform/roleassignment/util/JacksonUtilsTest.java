1579f03938e0e10d9ee1a509c59ed1dd50769b32
Sankar Padakula <sankar.padakula@hmcts.net>
2021-07-27 14:26:45 +0100

    mapping rules with rolename and category

--- src/test/java/uk/gov/hmcts/reform/roleassignment/util/JacksonUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/util/JacksonUtilsTest.java
@@ -1,8 +1,16 @@
 package uk.gov.hmcts.reform.roleassignment.util;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 
 class JacksonUtilsTest {
 
@@ -14,5 +22,21 @@ class JacksonUtilsTest {
     @Test
     void getHashMapTypeReference() {
         assertNotNull(JacksonUtils.getHashMapTypeReference());
+    }
+
+    @Test
+    void verifyStaticBlockFileNotFound() {
+        MockedStatic<Files> classMock = Mockito.mockStatic(Files.class);
+        classMock.when(() -> Files.walk(any(Path.class))).thenThrow(IOException.class);
+        assertEquals(JacksonUtils.getConfiguredRoles().get("roles").size(), 0);
+        classMock.close();
+    }
+
+    @Test
+    void verifyStaticBlockStreamFailed() {
+        MockedStatic<Files> classMock = Mockito.mockStatic(Files.class);
+        classMock.when(() -> Files.newInputStream(any(Path.class))).thenThrow(IOException.class);
+        assertEquals(JacksonUtils.getConfiguredRoles().get("roles").size(), 0);
+        classMock.close();
     }
 }
