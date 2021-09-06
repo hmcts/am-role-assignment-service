508353e0479d3acc7893e6e1e325f0896048e1d9
dharmendra8186 <dharmendra.singh@hmcts.net>
2021-09-06 13:46:58 +0100

    AM-1611- remove public identifier.

--- src/test/java/uk/gov/hmcts/reform/roleassignment/util/CustomLowerCaseDeserializerTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/util/CustomLowerCaseDeserializerTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class CustomLowerCaseDeserializerTest {
+class CustomLowerCaseDeserializerTest {
     private ObjectMapper mapper;
     private CustomLowerCaseDeserializer deserializer;
 
