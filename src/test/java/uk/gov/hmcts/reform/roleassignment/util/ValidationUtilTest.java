ef2df3a9eb9d7612fa4579557ff60d994bb11e8b
Dan Lysiak <daniel.lysiak1@hmcts.net>
2021-10-13 16:33:47 +0100

    unused import

--- src/test/java/uk/gov/hmcts/reform/roleassignment/util/ValidationUtilTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/util/ValidationUtilTest.java
@@ -17,7 +17,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
