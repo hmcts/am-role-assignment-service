4d2f1f0350acbee656c652c60096a042fbb1f850
Sankar Padakula <sankar.padakula@hmcts.net>
2022-02-03 17:53:08 +0000

    removed none GrantType

--- src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/enums/GrantType.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/enums/GrantType.java
@@ -1,5 +1,5 @@
 package uk.gov.hmcts.reform.roleassignment.domain.model.enums;
 
 public enum GrantType {
-    BASIC, SPECIFIC, STANDARD, CHALLENGED, EXCLUDED, NONE
+    BASIC, SPECIFIC, STANDARD, CHALLENGED, EXCLUDED
 }
