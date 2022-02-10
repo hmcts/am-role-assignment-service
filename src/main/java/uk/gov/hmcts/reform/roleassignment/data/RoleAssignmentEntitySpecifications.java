3418e1d3b73281bf928c52ab72f0c68020572d7d
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2022-02-10 16:53:59 +0000

    new index and query function

--- src/main/java/uk/gov/hmcts/reform/roleassignment/data/RoleAssignmentEntitySpecifications.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/data/RoleAssignmentEntitySpecifications.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.roleassignment.data;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.json.JSONObject;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.Predicate;
@@ -55,36 +56,24 @@ public final class RoleAssignmentEntitySpecifications {
 
             return null;
         }
-        return (root, query, builder) -> builder.and(attributes.entrySet()
-                                                         .stream()
-                                                         .map(entry -> builder.or(entry.getValue()
-                                                                              .stream()
-                                                       .map(value -> {
-                                                           if (value == null) {
-                                                               return builder.isNull(builder.function(
-                                                                                     "jsonb_extract_path_text",
-                                                                                      String.class,
-                                                                                      root.<String>get("attributes"),
-                                                                                      builder.literal(entry.getKey())
-                                                                                              ));
-                                                           } else {
-                                                               return builder.or(builder.equal(
-                                                                                                  builder.function(
-                                                                                      "jsonb_extract_path_text",
-                                                                                                      String.class,
-                                                                                                      root.<String>get(
-                                                                                                        "attributes"),
-                                                                                                      builder.literal(
-                                                                                                     entry.getKey())
-                                                                                                  ),
-                                                                                                  value
-                                                                                              ));
-                                                           }
-
-                                                       }).toArray(Predicate[]::new)))
+        return (root, query, builder) -> builder.and(attributes.entrySet().stream()
+                                                         .map(entry -> builder.or(entry.getValue().stream().map(value -> {
+                                                             if (value == null) {
+                                                                 return builder.isNull(builder.function(
+                                                                     "jsonb_extract_path_text",
+                                                                     String.class,
+                                                                     root.<String>get("attributes"),
+                                                                     builder.literal(entry.getKey())
+                                                                 ));
+                                                             } else {
+                                                                 return builder.or(builder.isTrue(builder.function(
+                                                                     "contains_jsonb",
+                                                                     Boolean.class,
+                                                                     root.get("attributes"),
+                                                                     builder.literal(new JSONObject().put(entry.getKey(), value).toString()))));
+                                                             }
+                                                         }).toArray(Predicate[]::new)))
                                                          .toArray(Predicate[]::new));
-
-
 
     }
 
