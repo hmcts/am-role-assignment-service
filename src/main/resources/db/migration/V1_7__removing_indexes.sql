f7fc21fe07d3c52f0df3c4dcba419100eb92e7b9
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2022-02-10 15:36:13 +0000

    added the required index

--- src/main/resources/db/migration/V1_7__removing_indexes.sql
+++ src/main/resources/db/migration/V1_7__removing_indexes.sql
@@ -1,6 +1,8 @@
 --Remove all the indexes to unblock the migration failure
 
-DROP INDEX CONCURRENTLY idx_actor_role_type, idx_actor_role_name, idx_actor_attributes, idx_attributes_role_type,
+DROP INDEX concurrently if exists idx_actor_role_type, idx_actor_role_name, idx_actor_attributes, idx_attributes_role_type,
 idx_attributes_role_name, idx_role_type, idx_role_name, idx_classification, idx_grant_type, idx_role_category, idx_read_only,
 idx_begin_time, idx_end_time, idx_created, role_assignment_attributes_idx;
 
+--New Gin index on the attribute jsonb for any containment operator.
+CREATE INDEX concurrently role_assignment_attributes_idx ON role_assignment USING gin(attributes jsonb_path_ops);
