ae46a1f27c8982c154bbff393d953a7db71c3361
m_2064714 <volodymyr.kovalskyi@hmcts.net>
2022-02-11 16:23:58 +0000

    Improving the audit logs

--- src/main/java/uk/gov/hmcts/reform/roleassignment/auditlog/AuditInterceptor.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/auditlog/AuditInterceptor.java
@@ -63,7 +63,8 @@ public class AuditInterceptor extends HandlerInterceptorAdapter {
         context.setHttpStatus(response.getStatus());
         context.setHttpMethod(request.getMethod());
         context.setRequestPath(request.getRequestURI());
-        if ((context.getResponseTime() != null && context.getResponseTime() > 500) || LOG.isDebugEnabled()) {
+        if ((context.getResponseTime() != null && context.getResponseTime() > 500) ||
+            (context.getResponseTime() != null && context.getHttpStatus() == 422) || LOG.isDebugEnabled()) {
             context.setRequestPayload(new MutableHttpServletRequest(request).getBodyAsString());
         }
         return context;
