54c78bf0a7c1df2b8163777739b5745f71dc1420
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2022-02-15 15:39:21 +0000

    Increase the application delay

--- src/main/resources/db/migration/V1_7__adding_new_history_indexe.sql
+++ src/main/resources/db/migration/V1_7__adding_new_history_indexe.sql
@@ -1,4 +1,4 @@
 --Remove the previous index as it has not been used
-DROP INDEX idx_process_reference ON role_assignment_history USING btree (process, reference);
+DROP INDEX idx_process_reference;
 --Add new index with upper case process and reference combination
 CREATE INDEX CONCURRENTLY idx_process_reference_upper ON role_assignment_history USING btree (upper(process), upper(reference));
