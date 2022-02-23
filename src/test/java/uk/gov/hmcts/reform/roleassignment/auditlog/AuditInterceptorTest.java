0e4ac19697243144e04b6691f7dc23928a5eb21b
m_2064714 <volodymyr.kovalskyi@hmcts.net>
2022-02-23 09:34:08 +0000

    Style check fix

--- src/test/java/uk/gov/hmcts/reform/roleassignment/auditlog/AuditInterceptorTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/auditlog/AuditInterceptorTest.java
@@ -22,7 +22,6 @@ import uk.gov.hmcts.reform.roleassignment.ApplicationParams;
 import uk.gov.hmcts.reform.roleassignment.auditlog.aop.AuditContext;
 import uk.gov.hmcts.reform.roleassignment.auditlog.aop.AuditContextHolder;
 
-import static org.junit.Assert.assertFalse;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
