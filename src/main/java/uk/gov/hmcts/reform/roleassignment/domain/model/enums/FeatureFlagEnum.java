d713af3514d68915559911aa9b1c9ce5ad985694
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2022-03-18 17:10:23 +0000

    added sscs judicial case validation rules

--- src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/enums/FeatureFlagEnum.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/enums/FeatureFlagEnum.java
@@ -1,9 +1,10 @@
 package uk.gov.hmcts.reform.roleassignment.domain.model.enums;
 
 public enum FeatureFlagEnum {
-    IAC_1_0("iac_1_0"), IAC_1_1("iac_1_1"), CCD_1_0("ccd_1_0"),IAC_JRD_1_0("iac_jrd_1_0"),
+    IAC_1_0("iac_1_0"), IAC_1_1("iac_1_1"),
+    CCD_1_0("ccd_1_0"),IAC_JRD_1_0("iac_jrd_1_0"),
     CCD_BYPASS_1_0("ccd_bypass_1_0"), IAC_SPECIFIC_1_0("iac_specific_1_0"),
-    IAC_CHALLENGED_1_0("iac_challenged_1_0");
+    IAC_CHALLENGED_1_0("iac_challenged_1_0"), SSCS_WA_1_0("sscs_wa_1_0");
 
     private final String value;
 
