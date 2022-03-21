e5a63ecf537d751e456e4d5b0852f1b42ab55275
Sankar Padakula <sankar.padakula@hmcts.net>
2022-03-21 16:15:22 +0000

    substantive flg script

---
+++ src/main/resources/db/migration/V1_10__iac_substantive_flag_config_tables.sql
@@ -0,0 +1,4 @@
+update role_assignment set attributes = "attributes" || '{"substantive":"Y"}' where role_type = 'CASE';
+
+update role_assignment set attributes = "attributes" || '{"substantive":"N"}' where role_type = 'CASE' and
+role_name in ('tribunal-caseworker', '[CREATOR]', '[CLAIMANT]', '[DEFENDANT]', '[APPLICANTTWO]');
