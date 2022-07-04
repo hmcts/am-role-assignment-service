4980816fdd2442367d717588dea3d075ae0064bf
m_2064714 <volodymyr.kovalskyi@hmcts.net>
2022-07-04 14:44:19 +0100

    Init Commit

--- src/test/java/uk/gov/hmcts/reform/roleassignment/ApplicationParamsTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/ApplicationParamsTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.roleassignment;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,7 +14,7 @@ class ApplicationParamsTest {
 
     @Test
     void shouldGetAuditLogEnabled() {
-        List<Integer> statusCodes = Arrays.asList(404);
+        List<Integer> statusCodes = List.of(404);
         ReflectionTestUtils.setField(applicationParams, "auditLogIgnoreStatuses", statusCodes);
         List<Integer> result = applicationParams.getAuditLogIgnoreStatuses();
         assertNotNull(result);
