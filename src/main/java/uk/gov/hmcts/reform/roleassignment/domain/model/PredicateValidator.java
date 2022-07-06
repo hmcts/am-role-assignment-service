b530ffdc11757ef33230f110cb34ca3c8bd9f2d9
arshinsalim <arshin.salim@hmcts.net>
2022-07-06 10:42:21 +0100

    Added with predicates

--- src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/PredicateValidator.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/domain/model/PredicateValidator.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.reform.roleassignment.domain.model;
 
 import java.util.function.Predicate;
 
+import uk.gov.hmcts.reform.roleassignment.domain.model.enums.Status;
+
 public class PredicateValidator {
 
     private PredicateValidator() {
@@ -11,4 +13,10 @@ public class PredicateValidator {
 
         return name -> name.equalsIgnoreCase(value);
     }
+
+    public static Predicate<Status> assignmentRequestPredicate(Status assignmentRequestStatus) {
+
+        return status -> status.equals(assignmentRequestStatus);
+    }
+
 }
