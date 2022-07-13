c8e3127bf67841fb4eb9bb1b0b11096bad689892
arshinsalim <arshin.salim@hmcts.net>
2022-07-13 11:56:57 +0100

    removal of iac_1_0 flag deletion

--- src/contractTest/java/uk/gov/hmcts/reform/roleassignment/DeleteRoleAssignmentProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/roleassignment/DeleteRoleAssignmentProviderTest.java
@@ -24,6 +24,7 @@ import uk.gov.hmcts.reform.roleassignment.domain.model.Case;
 import uk.gov.hmcts.reform.roleassignment.domain.model.ExistingRoleAssignment;
 import uk.gov.hmcts.reform.roleassignment.domain.model.Request;
 import uk.gov.hmcts.reform.roleassignment.domain.model.RoleAssignment;
+import uk.gov.hmcts.reform.roleassignment.domain.model.enums.FeatureFlagEnum;
 import uk.gov.hmcts.reform.roleassignment.domain.model.enums.RoleType;
 import uk.gov.hmcts.reform.roleassignment.domain.model.enums.Status;
 import uk.gov.hmcts.reform.roleassignment.domain.service.common.PersistenceService;
@@ -126,6 +127,7 @@ public class DeleteRoleAssignmentProviderTest {
             .thenReturn(TestDataBuilder.buildRequestEntity(
                 TestDataBuilder.buildRequest(Status.LIVE, false)));
         when(securityUtils.getServiceName()).thenReturn("am_org_role_mapping_service");
+        when(persistenceService.getStatusByParam(FeatureFlagEnum.IAC_1_1.getValue(), "pr")).thenReturn(true);
         when(securityUtils.getUserId()).thenReturn(AUTH_USER_ID);
 
         JsonNode attributes = buildAttributesFromFile("attributesCase.json");
