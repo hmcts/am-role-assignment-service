eecb5ccd23aadfa6a27f96c531af50fcb732a5d5
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2023-04-18 11:35:05 +0100

    AM-2755: Configure Private Law Tribunal caseworker to include decision_making_work workType

---
+++ src/main/resources/db/migration/V2_6__private_law_tribunal_caseworker_decision_making_work_addition.sql
@@ -0,0 +1,7 @@
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('prl_trib_1_0', 'local', 'private_law', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('prl_trib_1_0', 'pr', 'private_law', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('prl_trib_1_0', 'aat', 'private_law', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('prl_trib_1_0', 'demo', 'private_law', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('prl_trib_1_0', 'perftest', 'private_law', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('prl_trib_1_0', 'ithc', 'private_law', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('prl_trib_1_0', 'prod', 'private_law', 'false');
