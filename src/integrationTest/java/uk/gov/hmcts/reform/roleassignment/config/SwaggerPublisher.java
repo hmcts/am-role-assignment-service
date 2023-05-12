0bae3bc7535bc74f06c7b011d854c69fff8772d7
Mike Brown <michael.brown@hmcts.net>
2023-05-12 16:54:11 +0100

    checkstyle fix

--- src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/config/SwaggerPublisher.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/config/SwaggerPublisher.java
@@ -13,7 +13,7 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
