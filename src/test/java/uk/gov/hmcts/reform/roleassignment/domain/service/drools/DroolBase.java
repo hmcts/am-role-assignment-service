c70de7b3b11d57b4bc5bfbef5d28c5573390a2af
Abul Syed <abul.syed@HMCTS.net>
2023-06-05 11:38:25 +0100

    AM-2721:ST_CIC_WA - fixed checkstyle err

--- src/test/java/uk/gov/hmcts/reform/roleassignment/domain/service/drools/DroolBase.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/domain/service/drools/DroolBase.java
@@ -16,7 +16,11 @@ import uk.gov.hmcts.reform.roleassignment.helper.TestDataBuilder;
 
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-import static uk.gov.hmcts.reform.roleassignment.feignclients.configuration.DataStoreApiFallback.*;
+import static uk.gov.hmcts.reform.roleassignment.feignclients.configuration.DataStoreApiFallback.CIVIL_CASE_ID;
+import static uk.gov.hmcts.reform.roleassignment.feignclients.configuration.DataStoreApiFallback.PRIVATE_LAW_CASE_ID;
+import static uk.gov.hmcts.reform.roleassignment.feignclients.configuration.DataStoreApiFallback.PUBLIC_LAW_CASE_ID;
+import static uk.gov.hmcts.reform.roleassignment.feignclients.configuration.DataStoreApiFallback.SPECIALTRIBUNALS_CASE_ID;
+import static uk.gov.hmcts.reform.roleassignment.feignclients.configuration.DataStoreApiFallback.SSCS_CASE_ID;
 
 import java.util.ArrayList;
 import java.util.Collections;
