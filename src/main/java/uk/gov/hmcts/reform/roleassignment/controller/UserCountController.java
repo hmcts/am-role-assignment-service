4a42d62568d1237fd1963ef4b4434b783a24773b
Mike Brown <michael.brown@hmcts.net>
2023-09-21 17:03:13 +0100

    checkstyle fixes

---
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/controller/UserCountController.java
@@ -0,0 +1,64 @@
+
+package uk.gov.hmcts.reform.roleassignment.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.microsoft.applicationinsights.TelemetryClient;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.roleassignment.data.RoleAssignmentRepository;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static uk.gov.hmcts.reform.roleassignment.util.Constants.SERVICE_AUTHORIZATION2;
+
+@Slf4j
+@RestController
+public class UserCountController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserCountController.class);
+
+    @Autowired
+    private TelemetryClient telemetryClient;
+
+    @Autowired
+    private RoleAssignmentRepository roleAssignmentRepository;
+
+    @GetMapping(
+        path = "/am/role-assignments/user-count"
+    )
+    @Operation(summary = "Get User Count",
+        security =
+        {
+            @SecurityRequirement(name = AUTHORIZATION),
+            @SecurityRequirement(name = SERVICE_AUTHORIZATION2)
+        })
+    public ResponseEntity<Map<String, Object>> getUserCount() throws SQLException, JsonProcessingException {
+        List<RoleAssignmentRepository.JurisdictionRoleCategoryAndCount> userCount =
+            roleAssignmentRepository.getUserCount();
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Map<String, Object> counts = new HashMap<>();
+        counts.put("UserCount1", userCount);
+
+        List<RoleAssignmentRepository.JurisdictionRoleCategoryNameAndCount> userCount2 =
+            roleAssignmentRepository.getUserCount2();
+        String json2 = ow.writeValueAsString(userCount2);
+        counts.put("UserCount2", userCount2);
+        logger.warn(ow.writeValueAsString(counts));
+        telemetryClient.trackEvent(ow.writeValueAsString(counts));
+        return ResponseEntity.status(HttpStatus.OK).body(counts);
+    }
+}
--- src/main/java/uk/gov/hmcts/reform/roleassignment/data/RoleAssignmentRepository.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/data/RoleAssignmentRepository.java
@@ -1,11 +1,15 @@
 
 package uk.gov.hmcts.reform.roleassignment.data;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigInteger;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -16,6 +20,51 @@ public interface RoleAssignmentRepository extends JpaRepository<RoleAssignmentEn
     Set<RoleAssignmentEntity> findByActorId(String actorId) throws SQLException;
 
     void deleteByActorId(String actorId);
+
+    @Query(
+        value = "SELECT"
+            + "   jsonb_extract_path_text(ATTRIBUTES,'jurisdiction') AS jurisdiction,"
+            + "   role_category,"
+            + "   count(DISTINCT actor_id) AS count"
+            + "  FROM role_assignment"
+            + " WHERE role_type='ORGANISATION'"
+            + " GROUP BY jsonb_extract_path_text(attributes ,'jurisdiction'), role_category"
+            + " ORDER BY jsonb_extract_path_text(attributes ,'jurisdiction') asc, role_category asc;",
+        nativeQuery = true)
+    List<JurisdictionRoleCategoryAndCount> getUserCount() throws SQLException;
+
+    @Query(
+        value = "SELECT"
+            + "   jsonb_extract_path_text(ATTRIBUTES,'jurisdiction') AS jurisdiction,"
+            + "   role_category,"
+            + "   role_name,"
+            + "   count(DISTINCT actor_id) AS count"
+            + "  FROM role_assignment"
+            + " WHERE role_type='ORGANISATION'"
+            + " GROUP BY jsonb_extract_path_text(attributes ,'jurisdiction'), role_category, role_name"
+            + " ORDER BY jsonb_extract_path_text(attributes ,'jurisdiction') asc, role_category ASC, role_name ASC;",
+        nativeQuery = true)
+    List<JurisdictionRoleCategoryNameAndCount> getUserCount2() throws SQLException;
+
+    @JsonPropertyOrder({ "jurisdiction", "count"})
+    public interface JurisdictionRoleCategoryAndCount {
+        String getJurisdiction();
+
+        String getRoleCategory();
+
+        BigInteger getCount();
+    }
+
+    @JsonPropertyOrder({ "jurisdiction", "role_category", "role_name", "count"})
+    public interface JurisdictionRoleCategoryNameAndCount {
+        String getJurisdiction();
+
+        String getCategoryType();
+
+        String getRoleName();
+
+        BigInteger getCount();
+    }
 
 
 }
