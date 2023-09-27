c43caf0d68b3fef63a6fa0f649a374cbd99d588a
Mike Brown <michael.brown@hmcts.net>
2023-09-27 09:18:17 +0100

    unused imports removed

--- src/main/java/uk/gov/hmcts/reform/roleassignment/controller/UserCountController.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/controller/UserCountController.java
@@ -9,8 +9,6 @@ import com.microsoft.applicationinsights.TelemetryClient;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
