8340e9df7b355ec83f6d657e95d8506d88834205
Mike Brown <michael.brown@hmcts.net>
2023-11-03 12:25:03 +0000

    AM-2995 Provision Terraform Postgres v15 - subnet_suffix = "expanded"

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -126,6 +126,9 @@ module "role-assignment-database-v15" {
   common_tags        = var.common_tags
   pgsql_version      = "15"
 
+  # The original subnet is full, this is required to use the new subnet for new databases
+  subnet_suffix = "expanded"
+
   pgsql_databases = [
       {
         name = var.database_name
