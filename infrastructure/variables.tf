6cbd18a588121a3d1148e2f631d0451f8c0dfdbc
Mike Brown <michael.brown@hmcts.net>
2023-11-09 16:48:16 +0000

    Expanded Subnet added, made identical to perftest

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -62,3 +62,5 @@ variable "jenkins_AAD_objectId" {
   type        = string
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
+
+
