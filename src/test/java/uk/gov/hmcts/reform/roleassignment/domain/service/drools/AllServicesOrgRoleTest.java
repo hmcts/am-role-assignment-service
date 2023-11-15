29a643208d8581b37ed58a64b2f27e53c3265b32
Abul Syed <abul.syed@HMCTS.net>
2023-11-15 10:15:33 +0000

    GA-7 unit test

--- src/test/java/uk/gov/hmcts/reform/roleassignment/domain/service/drools/AllServicesOrgRoleTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/domain/service/drools/AllServicesOrgRoleTest.java
@@ -1069,4 +1069,93 @@ class AllServicesOrgRoleTest extends DroolBase {
         });
     }
 
+    @ParameterizedTest
+    @CsvSource({
+        "Role1,PROFESSIONAL,SSCS,Benefit,SSCS:all-cases:123:12345"
+    })
+    void shouldApproveOrgRequestedRoleByPrm(String roleName, String roleCategory, String jurisdiction,
+                                                    String caseType, String caseAccessGroupId) {
+        assignmentRequest.getRequest().setClientId("prm");
+        assignmentRequest.getRequest().setProcess("prm-system-user");
+        assignmentRequest.getRequest().setReference("prm-system-user-create");
+        assignmentRequest.getRequest().setReplaceExisting(true);
+        assignmentRequest.setRequestedRoles(getRequestedOrgRole());
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            roleAssignment.setRoleCategory(RoleCategory.valueOf(roleCategory));
+            roleAssignment.setRoleType(RoleType.ORGANISATION);
+            roleAssignment.setRoleName(roleName);
+            roleAssignment.setGrantType(STANDARD);
+            roleAssignment.setStatus(CREATE_REQUESTED);
+            roleAssignment.getAttributes().put("jurisdiction", convertValueJsonNode(jurisdiction));
+            roleAssignment.getAttributes().put("caseType", convertValueJsonNode(caseType));
+            roleAssignment.getAttributes().put("caseAccessGroupId", convertValueJsonNode(caseAccessGroupId));
+        });
+
+        buildExecuteKieSession();
+
+        //assertion
+        assertFalse(assignmentRequest.getRequest().isByPassOrgDroolRule());
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            assertEquals(APPROVED, roleAssignment.getStatus());
+            assertEquals(roleName, roleAssignment.getRoleName());
+            assertEquals(jurisdiction, roleAssignment.getAttributes().get("jurisdiction").asText());
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "Role1,PROFESSIONAL,SSCS"
+    })
+    void shouldDeleteOrgRequestedRoleByPrm(String roleName, String roleCategory, String jurisdiction) {
+        assignmentRequest.getRequest().setClientId("prm");
+        assignmentRequest.setRequestedRoles(getRequestedOrgRole());
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            roleAssignment.setRoleCategory(RoleCategory.valueOf(roleCategory));
+            roleAssignment.setRoleType(RoleType.ORGANISATION);
+            roleAssignment.setRoleName(roleName);
+            roleAssignment.setGrantType(STANDARD);
+            roleAssignment.setStatus(Status.DELETE_REQUESTED);
+            roleAssignment.getAttributes().put("jurisdiction", convertValueJsonNode(jurisdiction));
+        });
+
+        buildExecuteKieSession();
+
+        //assertion
+        assertFalse(assignmentRequest.getRequest().isByPassOrgDroolRule());
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            assertEquals(DELETE_APPROVED, roleAssignment.getStatus());
+            assertEquals(roleName, roleAssignment.getRoleName());
+            assertEquals(jurisdiction, roleAssignment.getAttributes().get("jurisdiction").asText());
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "Role1,PROFESSIONAL,SSCS,Benefit"
+    })
+    void shouldRejectOrgRequestedRoleWithMissingCaseAccessGroupIdByPrm(String roleName, String roleCategory,
+                                                                     String jurisdiction, String caseType) {
+        assignmentRequest.getRequest().setClientId("prm");
+        assignmentRequest.getRequest().setProcess("prm-system-user");
+        assignmentRequest.getRequest().setReference("prm-system-user-create");
+        assignmentRequest.getRequest().setReplaceExisting(true);
+        assignmentRequest.setRequestedRoles(getRequestedOrgRole());
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            roleAssignment.setRoleCategory(RoleCategory.valueOf(roleCategory));
+            roleAssignment.setRoleType(RoleType.ORGANISATION);
+            roleAssignment.setRoleName(roleName);
+            roleAssignment.setGrantType(STANDARD);
+            roleAssignment.setStatus(CREATE_REQUESTED);
+            roleAssignment.getAttributes().put("jurisdiction", convertValueJsonNode(jurisdiction));
+            roleAssignment.getAttributes().put("caseType", convertValueJsonNode(caseType));
+        });
+
+        buildExecuteKieSession();
+
+        //assertion
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            assertEquals(Status.REJECTED, roleAssignment.getStatus());
+        });
+    }
+
 }
