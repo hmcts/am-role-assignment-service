1e1c8e90fa40766c78f1305f7d20d87f352df385
Mike Brown <michael.brown@hmcts.net>
2023-12-29 14:24:16 +0000

    AM-3114 Remove IDAM Dependency from RAS - Integration test working still using junit 4

--- src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/BaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/BaseTest.java
@@ -1,8 +1,11 @@
 package uk.gov.hmcts.reform.roleassignment;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.github.tomakehurst.wiremock.WireMockServer;
 import com.opentable.db.postgres.embedded.EmbeddedPostgres;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
@@ -10,10 +13,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.lang.NonNull;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+import uk.gov.hmcts.reform.roleassignment.controller.utils.WiremockFixtures;
 import uk.gov.hmcts.reform.roleassignment.health.CcdDataStoreHealthIndicator;
 import uk.gov.hmcts.reform.roleassignment.health.IdamServiceHealthIndicator;
 
@@ -26,10 +40,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = {BaseTest.WireMockServerInitializer.class})
+@ActiveProfiles("itest")
 public abstract class BaseTest {
+
+    public static final WireMockServer WIRE_MOCK_SERVER = new WireMockServer(options().dynamicPort());
 
     protected static final ObjectMapper mapper = new ObjectMapper();
 
@@ -38,6 +58,23 @@ public abstract class BaseTest {
 
     @MockBean
     CcdDataStoreHealthIndicator ccdDataStoreHealthIndicator;
+
+    @MockBean(name = "clientRegistrationRepository")
+    private ClientRegistrationRepository getClientRegistrationRepository;
+
+    @MockBean(name = "reactiveClientRegistrationRepository")
+    private ReactiveClientRegistrationRepository getReactiveClientRegistrationRepository;
+
+    static {
+        if (!WIRE_MOCK_SERVER.isRunning()) {
+            WIRE_MOCK_SERVER.start();
+        }
+
+        mapper.registerModule(new JavaTimeModule());
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+        // Force re-initialisation of base types for each test suite
+    }
 
     @BeforeClass
     public static void init() {
@@ -80,6 +117,33 @@ public abstract class BaseTest {
             if (connection != null) {
                 connection.close();
             }
+        }
+    }
+
+    public static class WireMockServerInitializer
+        implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        private final WiremockFixtures wiremockFixtures = new WiremockFixtures();
+
+        @Override
+        public void initialize(@NonNull ConfigurableApplicationContext applicationContext) {
+
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+                applicationContext,
+                "wiremock.server.port=" + WIRE_MOCK_SERVER.port()
+            );
+
+            try {
+                wiremockFixtures.stubIdamConfig();
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+
+            applicationContext.addApplicationListener((ApplicationListener<ContextClosedEvent>) event -> {
+                if (WIRE_MOCK_SERVER.isRunning()) {
+                    WIRE_MOCK_SERVER.shutdown();
+                }
+            });
         }
     }
 }
