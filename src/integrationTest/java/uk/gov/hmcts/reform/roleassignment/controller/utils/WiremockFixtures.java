f9dd8a2498f348829aca8bc21dbec026322d81b4
Mike Brown <michael.brown@hmcts.net>
2024-01-12 16:21:06 +0000

    unused imports removed

--- src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/controller/utils/WiremockFixtures.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/controller/utils/WiremockFixtures.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.roleassignment.controller.utils;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
