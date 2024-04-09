01a43b019abe4690f1ac4d7d265ca2599455ea32
Mike Brown <michael.brown@hmcts.net>
2024-04-09 11:46:00 +0100

    AM-3035 Schema Owner Trigger Prod

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -127,7 +127,7 @@ module "role-assignment-database-v15" {
   pgsql_version      = "15"
 
   # Setup Access Reader db user
-  force_user_permissions_trigger = "3"
+  force_user_permissions_trigger = "4"
 
   # Sets correct DB owner after migration to fix permissions
   enable_schema_ownership = var.enable_schema_ownership
