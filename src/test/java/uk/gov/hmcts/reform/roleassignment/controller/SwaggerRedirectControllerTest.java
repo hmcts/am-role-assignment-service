90d1268dbfc724cdee3ffd535e726222526c2407
Mike Brown <michael.brown@hmcts.net>
2024-06-05 10:30:23 +0100

    whitespace and commented code removed

--- src/test/java/uk/gov/hmcts/reform/roleassignment/controller/SwaggerRedirectControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/controller/SwaggerRedirectControllerTest.java
@@ -7,7 +7,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static org.springdoc.core.Constants.SWAGGER_UI_URL;
 
 @RunWith(MockitoJUnitRunner.class)
 class SwaggerRedirectControllerTest {
