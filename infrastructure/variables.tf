6c0a59b2c72f6ca72842bb2abe67aa42a597d527
Matt Nayler <57350764+mattnayler@users.noreply.github.com>
2024-09-09 16:45:49 +0100

    DTSAM-516 Resize storage for RAS DB in AAT (#2331)
    * DTSAM-516 Resize storage for RAS DB in AAT
    
       [DTSAM-516](https://tools.hmcts.net/jira/browse/DTSAM-516)
    
    Resize storage for RAS DB in AAT.
    
    * DTSAM-516 Resize storage for RAS DB in AAT

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -85,3 +85,9 @@ variable "pgsql_sku" {
   description = "The PGSql flexible server instance sku"
   default     = "GP_Standard_D2s_v3"
 }
+
+variable "pgsql_storage_mb" {
+  description = "Max storage allowed for the PGSql Flexibile instance"
+  type        = number
+  default     = 65536
+}
