2e200a7b2e690b445fc0e223c5dd59a88af9329a
Abul Syed <abul.syed@HMCTS.net>
2024-09-18 10:42:57 +0100

    DTSAM-531 case-allocator role config for IAC System user

--- src/test/java/uk/gov/hmcts/reform/roleassignment/domain/service/drools/AllServicesOrgRoleTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/domain/service/drools/AllServicesOrgRoleTest.java
@@ -910,4 +910,94 @@ class AllServicesOrgRoleTest extends DroolBase {
         });
     }
 
+    @ParameterizedTest
+    @CsvSource({
+        "case-allocator,SYSTEM,IA,UK"
+    })
+    void shouldApproveOrgRequestedRoleForIacCaseAllocator(String roleName, String roleCategory,
+                                                          String jurisdiction, String primaryLocation) {
+        assignmentRequest.getRequest().setClientId("iac");
+        assignmentRequest.getRequest().setProcess("iac-system-users");
+        assignmentRequest.getRequest().setReference("iac-case-allocator-system-user");
+        assignmentRequest.getRequest().setReplaceExisting(true);
+        assignmentRequest.setRequestedRoles(getRequestedOrgRole());
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            roleAssignment.setRoleCategory(RoleCategory.valueOf(roleCategory));
+            roleAssignment.setRoleType(RoleType.ORGANISATION);
+            roleAssignment.setRoleName(roleName);
+            roleAssignment.setGrantType(STANDARD);
+            roleAssignment.setStatus(CREATE_REQUESTED);
+            roleAssignment.getAttributes().put("jurisdiction", convertValueJsonNode(jurisdiction));
+            roleAssignment.getAttributes().put("primaryLocation", convertValueJsonNode(primaryLocation));
+        });
+
+        buildExecuteKieSession();
+
+        //assertion
+        assertFalse(assignmentRequest.getRequest().isByPassOrgDroolRule());
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            assertEquals(APPROVED, roleAssignment.getStatus());
+            assertEquals(roleName, roleAssignment.getRoleName());
+            assertEquals(jurisdiction, roleAssignment.getAttributes().get("jurisdiction").asText());
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "case-allocator,SYSTEM,IA"
+    })
+    void shouldDeleteOrgRequestedRoleForIacCaseAllocator(String roleName, String roleCategory, String jurisdiction) {
+        assignmentRequest.getRequest().setClientId("iac");
+        assignmentRequest.setRequestedRoles(getRequestedOrgRole());
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            roleAssignment.setRoleCategory(RoleCategory.valueOf(roleCategory));
+            roleAssignment.setRoleType(RoleType.ORGANISATION);
+            roleAssignment.setRoleName(roleName);
+            roleAssignment.setGrantType(STANDARD);
+            roleAssignment.setStatus(Status.DELETE_REQUESTED);
+            roleAssignment.getAttributes().put("jurisdiction", convertValueJsonNode(jurisdiction));
+        });
+
+        buildExecuteKieSession();
+
+        //assertion
+        assertFalse(assignmentRequest.getRequest().isByPassOrgDroolRule());
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            assertEquals(DELETE_APPROVED, roleAssignment.getStatus());
+            assertEquals(roleName, roleAssignment.getRoleName());
+            assertEquals(jurisdiction, roleAssignment.getAttributes().get("jurisdiction").asText());
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "case-allocator,SYSTEM,PRIVATELAW,UK"
+    })
+    void shouldRejectIacOrgRequestedRoleForCaseAllocatorFromAnotherJurisdiction(String roleName,
+                                                                                String roleCategory,
+                                                                                String jurisdiction,
+                                                                                String primaryLocation) {
+        assignmentRequest.getRequest().setClientId("iac");
+        assignmentRequest.getRequest().setProcess("iac-system-users");
+        assignmentRequest.getRequest().setReference("iac-case-allocator-system-user");
+        assignmentRequest.getRequest().setReplaceExisting(true);
+        assignmentRequest.setRequestedRoles(getRequestedOrgRole());
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            roleAssignment.setRoleCategory(RoleCategory.valueOf(roleCategory));
+            roleAssignment.setRoleType(RoleType.ORGANISATION);
+            roleAssignment.setRoleName(roleName);
+            roleAssignment.setGrantType(STANDARD);
+            roleAssignment.setStatus(CREATE_REQUESTED);
+            roleAssignment.getAttributes().put("jurisdiction", convertValueJsonNode(jurisdiction));
+            roleAssignment.getAttributes().put("primaryLocation", convertValueJsonNode(primaryLocation));
+        });
+
+        buildExecuteKieSession();
+
+        //assertion
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            assertEquals(Status.REJECTED, roleAssignment.getStatus());
+        });
+    }
+
 }
