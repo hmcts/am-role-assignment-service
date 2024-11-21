7d21bbf34597dcde8062d526d51ce5508f8cd129
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2024-11-21 10:12:29 +0000

    CME-121: Set cpu, memory and storage threshold to 10 percent

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -99,19 +99,19 @@ variable "action_group_name" {
 }
 
 variable "cpu_threshold" {
-  default     = 70
+  default     = 10
   type        = number
   description = "Average CPU utilisation threshold"
 }
 
 variable "memory_threshold" {
-  default     = 70
+  default     = 10
   type        = number
   description = "Average memory utilisation threshold"
 }
 
 variable "storage_threshold" {
-  default     = 70
+  default     = 10
   type        = number
   description = "Average storage utilisation threshold"
 }
