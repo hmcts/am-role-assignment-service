5d757174fa4652f485cd0b7cec8b261858979a63
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2024-12-06 12:57:00 +0000

    CME-121: Change thresholds

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -99,19 +99,19 @@ variable "action_group_name" {
 }
 
 variable "cpu_threshold" {
-  default     = 5
+  default     = 70
   type        = number
   description = "Average CPU utilisation threshold"
 }
 
 variable "memory_threshold" {
-  default     = 5
+  default     = 70
   type        = number
   description = "Average memory utilisation threshold"
 }
 
 variable "storage_threshold" {
-  default     = 2
+  default     = 70
   type        = number
   description = "Average storage utilisation threshold"
 }
