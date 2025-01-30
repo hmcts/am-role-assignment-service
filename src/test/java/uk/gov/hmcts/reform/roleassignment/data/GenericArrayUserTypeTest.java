3fca5f6de412f458332f208bec1cad89e76c3471
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2025-01-30 14:20:58 +0000

    CME-56: fix sonar quality issue

--- src/test/java/uk/gov/hmcts/reform/roleassignment/data/GenericArrayUserTypeTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/data/GenericArrayUserTypeTest.java
@@ -153,7 +153,6 @@ public class GenericArrayUserTypeTest {
     @Test
     public void executeSqlTypes() {
         int response = sut.getSqlType();
-        assertNotNull(response);
         assertEquals(Types.ARRAY, response);
     }
 
