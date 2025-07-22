9d0ee882f634236095e2fe3f9952e2bc5ec8f74b
Matt Nayler <matt.nayler@cgi.com>
2025-07-22 16:11:14 +0100

    DTSAM-993 Update BEFTA PRM Role Config

--- src/test/java/uk/gov/hmcts/reform/roleassignment/domain/service/drools/PrmOrgRoleTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignment/domain/service/drools/PrmOrgRoleTest.java
@@ -21,20 +21,69 @@ import static uk.gov.hmcts.reform.roleassignment.helper.TestDataBuilder.getReque
 import static uk.gov.hmcts.reform.roleassignment.util.JacksonUtils.convertValueJsonNode;
 
 @RunWith(MockitoJUnitRunner.class)
-public class PrmOrgRoleTest extends DroolBase {
+class PrmOrgRoleTest extends DroolBase {
 
     @ParameterizedTest
     @CsvSource({
-        "Role1,PROFESSIONAL,SSCS,Benefit,SSCS:all-cases:123:12345"
+        "PRM_Test_Org_Role,TEST_JURISDICTION,TestCaseType"
     })
-    void shouldApproveProfessionalOrgRoleRequest(String roleName, String roleCategory, String jurisdiction,
-                                            String caseType, String caseAccessGroupId) {
+    void shouldApproveProfessionalOrgRoleRequest(String roleName,
+                                                 String jurisdiction,
+                                                 String caseType) {
+
+        // arrange
         assignmentRequest.getRequest().setClientId("am_org_role_mapping_service");
         assignmentRequest.getRequest().setProcess("professional-organisational-role-mapping");
         assignmentRequest.getRequest().setReplaceExisting(true);
         assignmentRequest.setRequestedRoles(getRequestedOrgRole());
         assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
-            roleAssignment.setRoleCategory(RoleCategory.valueOf(roleCategory));
+            roleAssignment.setRoleCategory(RoleCategory.PROFESSIONAL);
+            roleAssignment.setRoleType(RoleType.ORGANISATION);
+            roleAssignment.setRoleName(roleName);
+            roleAssignment.setGrantType(STANDARD);
+            roleAssignment.setClassification(RESTRICTED);
+            roleAssignment.setStatus(CREATE_REQUESTED);
+            roleAssignment.getAttributes().put("jurisdiction", convertValueJsonNode(jurisdiction));
+            roleAssignment.getAttributes().put("caseType", convertValueJsonNode(caseType));
+        });
+
+        FeatureFlag featureFlag = FeatureFlag.builder()
+            .flagName(FeatureFlagEnum.GA_PRM_1_0.getValue())
+            .status(true)
+            .build();
+        featureFlags.add(featureFlag);
+
+        // act
+        buildExecuteKieSession();
+
+        // assertion
+        assertFalse(assignmentRequest.getRequest().isByPassOrgDroolRule());
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            assertEquals(APPROVED, roleAssignment.getStatus());
+            assertEquals(RoleCategory.PROFESSIONAL, roleAssignment.getRoleCategory());
+            assertEquals(RoleType.ORGANISATION, roleAssignment.getRoleType());
+            assertEquals(roleName, roleAssignment.getRoleName());
+            assertEquals(jurisdiction, roleAssignment.getAttributes().get("jurisdiction").asText());
+            assertEquals(caseType, roleAssignment.getAttributes().get("caseType").asText());
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "PRM_Test_GA_Role,TEST_JURISDICTION,TestCaseType,TEST_JURISDICTION:all-cases:123:12345"
+    })
+    void shouldApproveProfessionalOrgGroupAccessRoleRequest(String roleName,
+                                                            String jurisdiction,
+                                                            String caseType,
+                                                            String caseAccessGroupId) {
+
+        // arrange
+        assignmentRequest.getRequest().setClientId("am_org_role_mapping_service");
+        assignmentRequest.getRequest().setProcess("professional-organisational-role-mapping");
+        assignmentRequest.getRequest().setReplaceExisting(true);
+        assignmentRequest.setRequestedRoles(getRequestedOrgRole());
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
+            roleAssignment.setRoleCategory(RoleCategory.PROFESSIONAL);
             roleAssignment.setRoleType(RoleType.ORGANISATION);
             roleAssignment.setRoleName(roleName);
             roleAssignment.setGrantType(STANDARD);
@@ -51,26 +100,34 @@ public class PrmOrgRoleTest extends DroolBase {
             .build();
         featureFlags.add(featureFlag);
 
+        // act
         buildExecuteKieSession();
 
-        //assertion
+        // assertion
         assertFalse(assignmentRequest.getRequest().isByPassOrgDroolRule());
         assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
             assertEquals(APPROVED, roleAssignment.getStatus());
+            assertEquals(RoleCategory.PROFESSIONAL, roleAssignment.getRoleCategory());
+            assertEquals(RoleType.ORGANISATION, roleAssignment.getRoleType());
             assertEquals(roleName, roleAssignment.getRoleName());
             assertEquals(jurisdiction, roleAssignment.getAttributes().get("jurisdiction").asText());
+            assertEquals(caseType, roleAssignment.getAttributes().get("caseType").asText());
+            assertEquals(caseAccessGroupId, roleAssignment.getAttributes().get("caseAccessGroupId").asText());
         });
     }
 
     @ParameterizedTest
     @CsvSource({
-        "Role1,PROFESSIONAL,SSCS"
+        "PRM_Test_Org_Role,TEST_JURISDICTION",
+        "PRM_Test_GA_Role,TEST_JURISDICTION"
     })
-    void shouldDeleteProfessionalOrgRole(String roleName, String roleCategory, String jurisdiction) {
+    void shouldDeleteProfessionalOrgRole(String roleName, String jurisdiction) {
+
+        // arrange
         assignmentRequest.getRequest().setClientId("am_org_role_mapping_service");
         assignmentRequest.setRequestedRoles(getRequestedOrgRole());
         assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
-            roleAssignment.setRoleCategory(RoleCategory.valueOf(roleCategory));
+            roleAssignment.setRoleCategory(RoleCategory.PROFESSIONAL);
             roleAssignment.setRoleType(RoleType.ORGANISATION);
             roleAssignment.setRoleName(roleName);
             roleAssignment.setGrantType(STANDARD);
@@ -79,12 +136,15 @@ public class PrmOrgRoleTest extends DroolBase {
             roleAssignment.getAttributes().put("jurisdiction", convertValueJsonNode(jurisdiction));
         });
 
+        // act
         buildExecuteKieSession();
 
-        //assertion
+        // assertion
         assertFalse(assignmentRequest.getRequest().isByPassOrgDroolRule());
         assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
             assertEquals(DELETE_APPROVED, roleAssignment.getStatus());
+            assertEquals(RoleCategory.PROFESSIONAL, roleAssignment.getRoleCategory());
+            assertEquals(RoleType.ORGANISATION, roleAssignment.getRoleType());
             assertEquals(roleName, roleAssignment.getRoleName());
             assertEquals(jurisdiction, roleAssignment.getAttributes().get("jurisdiction").asText());
         });
@@ -92,16 +152,19 @@ public class PrmOrgRoleTest extends DroolBase {
 
     @ParameterizedTest
     @CsvSource({
-        "Role1,PROFESSIONAL,SSCS,Benefit"
+        "PRM_Test_GA_Role,TEST_JURISDICTION,TestCaseType"
     })
-    void shouldRejectProfessionalOrgRoleRequestWithMissingCaseAccessGroupId(String roleName, String roleCategory,
-                                                                       String jurisdiction, String caseType) {
+    void shouldRejectProfessionalOrgGroupAccessRoleRequestWithMissingCaseAccessGroupId(String roleName,
+                                                                                       String jurisdiction,
+                                                                                       String caseType) {
+
+        // arrange
         assignmentRequest.getRequest().setClientId("am_org_role_mapping_service");
         assignmentRequest.getRequest().setProcess("professional-organisational-role-mapping");
         assignmentRequest.getRequest().setReplaceExisting(true);
         assignmentRequest.setRequestedRoles(getRequestedOrgRole());
         assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
-            roleAssignment.setRoleCategory(RoleCategory.valueOf(roleCategory));
+            roleAssignment.setRoleCategory(RoleCategory.PROFESSIONAL);
             roleAssignment.setRoleType(RoleType.ORGANISATION);
             roleAssignment.setRoleName(roleName);
             roleAssignment.setGrantType(STANDARD);
@@ -117,11 +180,12 @@ public class PrmOrgRoleTest extends DroolBase {
             .build();
         featureFlags.add(featureFlag);
 
+        // act
         buildExecuteKieSession();
 
-        //assertion
-        assignmentRequest.getRequestedRoles().forEach(roleAssignment -> {
-            assertEquals(Status.REJECTED, roleAssignment.getStatus());
-        });
+        // assertion
+        assignmentRequest.getRequestedRoles().forEach(roleAssignment ->
+                                                          assertEquals(Status.REJECTED, roleAssignment.getStatus()));
     }
+
 }
