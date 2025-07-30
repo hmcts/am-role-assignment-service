22614bf45ec733a9f7213c5385be233e2d3f0c40
lukasz-wolski <1005015+lukasz-wolski@users.noreply.github.com>
2025-07-30 12:39:45 +0100

    cleanup

--- src/main/java/uk/gov/hmcts/reform/roleassignment/RoleAssignmentApplication.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignment/RoleAssignmentApplication.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.context.annotation.Bean;
@@ -22,7 +21,6 @@ import java.time.Clock;
 
 @SpringBootApplication
 @EnableTransactionManagement(proxyTargetClass = true)
-//@EnableCircuitBreaker
 @EnableCaching
 @EnableScheduling
 @EnableRetry
