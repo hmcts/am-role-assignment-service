a9b078335e8d2643619e36e2187cd7ce77e4e7c9
Alex McAusland <a.m@user.com>
2025-11-14 12:15:33 +0000

    rename migraton and limit to local + pr envs

--- src/main/resources/db/migration/V20240724_372__DTSAM-372_PCS_CASE_ROLES_1_0_base_flag_addition.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'local', 'pcs', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'pr', 'pcs', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'aat', 'pcs', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'demo', 'pcs', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'perftest', 'pcs', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'ithc', 'pcs', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'prod', 'pcs', 'false');
---
+++ src/main/resources/db/migration/V20251114_1098__DTSAM-1098_PCS_CASE_ROLES_1_0_base_flag_addition.sql
@@ -0,0 +1,7 @@
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'local', 'pcs', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'pr', 'pcs', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'aat', 'pcs', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'demo', 'pcs', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'perftest', 'pcs', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'ithc', 'pcs', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('pcs_case_roles_1_0', 'prod', 'pcs', 'false');
