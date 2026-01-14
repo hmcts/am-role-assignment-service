7fbbac5e10114a33becf39974353ae4949b224dd
Łukasz Krzysztof Wolski <1005015+lukasz-wolski@users.noreply.github.com>
2026-01-14 14:40:26 +0000

    Update SwaggerPublisher.java

--- src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/config/SwaggerPublisher.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/config/SwaggerPublisher.java
@@ -42,7 +42,7 @@ public class SwaggerPublisher extends BaseTest {
     }
 
     @Test
-    @org.junit.Ignore
+    @org.junit.jupiter.api.Disabled
     public void generateDocs() throws Exception {
         byte[] specs = mockMvc.perform(get(DEFAULT_API_DOCS_URL))
             .andExpect(status().isOk())
