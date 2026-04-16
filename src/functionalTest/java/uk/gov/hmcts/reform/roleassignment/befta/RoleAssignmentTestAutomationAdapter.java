02978f5b52e097a19ef1d2cc639e1d46930a91d5
Matt Nayler <matt.nayler@cgi.com>
2026-04-16 15:21:16 +0100

    DTSAM-1103 FTAs for aac_manage_case_assignment case-role delete

--- src/functionalTest/java/uk/gov/hmcts/reform/roleassignment/befta/RoleAssignmentTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/roleassignment/befta/RoleAssignmentTestAutomationAdapter.java
@@ -24,6 +24,7 @@ public class RoleAssignmentTestAutomationAdapter extends DefaultTestAutomationAd
             case ("generateProcessId") -> generateProcessId("RAS_FTA");
             case ("generateS2STokenForCaseDisposer") -> super.getNewS2SToken("ccd_case_disposer");
             case ("generateS2STokenForCcd") -> super.getNewS2SToken("ccd_data");
+            case ("generateS2STokenForMca") -> super.getNewS2SToken("aac_manage_case_assignment");
             case ("generateS2STokenForOrm") -> super.getNewS2SToken("am_org_role_mapping_service");
             case ("generateS2STokenForXui") -> super.getNewS2SToken("xui_webapp");
             case ("tomorrow") -> LocalDate.now().plusDays(1);
