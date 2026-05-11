b2e5479655e9626b79bdd3813496773a0d8e5f62
padmapriyanalam <67232857+padmapriyanalam@users.noreply.github.com>
2026-05-11 10:41:18 +0100

    POFCC-101 & POFCC-103 Add Judicial & Staff role config for Possessions (#2687)
    * POFCC-101 Add Judicial role config for Possessions
    
    * POFCC-101 update unit tests
    
    * POFCC-101 update unit tests
    
    * POFCC-101 fix unit tests
    
    * POFCC-103 Create role configuration based on role category for Possessions STAFF users in RA
    
    * POFCC-103 Create role configuration based on role category for Possessions STAFF users in RA
    
    * POFCC-101 - fix merge failure
    
    ---------
    
    Co-authored-by: Matt Nayler <57350764+mattnayler@users.noreply.github.com>

--- src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/controller/RoleAssignmentIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/controller/RoleAssignmentIntegrationTest.java
@@ -249,7 +249,7 @@ class RoleAssignmentIntegrationTest extends BaseTest {
         List<RoleConfigRole> roleConfigRoles = mapper.readValue(response, new TypeReference<>() {
         });
 
-        assertEquals(226, roleConfigRoles.size());
+        assertEquals(228, roleConfigRoles.size());
         for (RoleConfigRole roleConfigRole : roleConfigRoles) {
             assertNotNull(roleConfigRole.getName());
             assertNotNull(roleConfigRole.getCategory());
