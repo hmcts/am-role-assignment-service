14edb8a6ea93c41bc390c095321a4dcf0c2a438a
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-07-14 15:42:13 +0100

    DTSAM-1278 Make persistenceService primary

--- src/contractTest/java/uk/gov/hmcts/reform/roleassignment/RoleAssignmentProviderTestConfiguration.java
+++ src/contractTest/java/uk/gov/hmcts/reform/roleassignment/RoleAssignmentProviderTestConfiguration.java
@@ -29,6 +29,7 @@ import static org.mockito.Mockito.when;
 public class RoleAssignmentProviderTestConfiguration {
 
     @Bean
+    @Primary
     public PersistenceService persistenceService() {
         return Mockito.mock(PersistenceService.class);
     }
