95851af9b63fdfd4d1335d5bec3231288a82edba
Ben James <ben.james@hmcts.net>
2026-07-15 14:46:21 +0100

    Additional naming change on feature flag, amended service from divorce to fr

--- src/main/resources/db/migration/V20260515_1227__COT-1227_FR_WA_1_0_base_flag_addition.sql
+++ src/main/resources/db/migration/V20260515_1227__COT-1227_FR_WA_1_0_base_flag_addition.sql
@@ -1,8 +1,8 @@
 -- insert fr_wa_1_0 base flag into flag_config table
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'local', 'divorce', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'pr', 'divorce', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'aat', 'divorce', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'demo', 'divorce', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'perftest', 'divorce', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'ithc', 'divorce', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'prod', 'divorce', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'local', 'fr', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'pr', 'fr', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'aat', 'fr', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'demo', 'fr', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'perftest', 'fr', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'ithc', 'fr', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('fr_wa_1_0', 'prod', 'fr', 'false');
