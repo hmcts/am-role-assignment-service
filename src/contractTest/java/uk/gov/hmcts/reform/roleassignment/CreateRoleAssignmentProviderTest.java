6a37701ba8e3c59f7d517acfcdcbccdd6e97e325
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-09-15 09:08:48 +0100

    DTSAM-1393 Add the same testPropertySource as deleteRoleAssignmentProviderTest.

--- src/contractTest/java/uk/gov/hmcts/reform/roleassignment/CreateRoleAssignmentProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/roleassignment/CreateRoleAssignmentProviderTest.java
@@ -51,7 +51,7 @@ import static uk.gov.hmcts.reform.roleassignment.helper.TestDataBuilder.buildAtt
 @PactBroker(scheme = "${PACT_BROKER_SCHEME:http}",
     host = "${PACT_BROKER_URL:localhost}", port = "${PACT_BROKER_PORT:9292}", consumerVersionSelectors = {
         @VersionSelector(tag = "master")})
-@TestPropertySource(properties = {"org.request.byPassOrgDroolRule=true", "roleassignment.query.size=20",
+@TestPropertySource(properties = {"roleassignment.query.size=20",
     "spring.cache.type=none", "ras.environment=pr", "roleassignment.query.sizeinternal=100"})
 @Import(RoleAssignmentProviderTestConfiguration.class)
 //@IgnoreNoPactsToVerify
